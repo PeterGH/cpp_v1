35bdb75d3ab1018025bc44c2b5c4071f3e75bf55
Peter <peteryzufl@gmail.com>
2019-11-16 22:02:06 -0800

    test

--- AlgorithmTest.h
+++ AlgorithmTest.h
@@ -4988,6 +4988,105 @@ void AlgorithmTest::Init(void)
         check("aabdbaabeeadcbbdedacbbeecbabebaeeecaeabaedadcbdbcdaabebdadbbaeabdadeaabbabbecebbebcaddaacccebeaeedababedeacdeaaaeeaecbe", "bddabdcae", 10582116);
     });
 
+    Add("Join", [&]() {
+        int i1[] = {0, 1, 2, 3};
+        string o1 = Test::String::Join(i1, 4, ":");
+        Logger().WriteInformation("%s\n", o1.c_str());
+        ASSERT1(0 == strcmp("0:1:2:3", o1.c_str()));
+
+        wstring o2 = Test::String::Join(i1, 4, L":");
+        Logger().WriteInformation("%S\n", o2.c_str());
+        ASSERT1(0 == wcscmp(L"0:1:2:3", o2.c_str()));
+
+        double i2[] = {0.1, 1.2, 2.3, 3.4};
+        string o3 = Test::String::Join(i2, 4, ":");
+        Logger().WriteInformation("%s\n", o3.c_str());
+        ASSERT1(0 == strcmp("0.1:1.2:2.3:3.4", o3.c_str()));
+
+        wstring o4 = Test::String::Join(i2, 4, L":");
+        Logger().WriteInformation("%S\n", o4.c_str());
+        ASSERT1(0 == wcscmp(L"0.1:1.2:2.3:3.4", o4.c_str()));
+    });
+
+    Add("WordLadder", [&]() {
+        auto check = [&](const string &start, const string &end, const unordered_set<string> &dict, int expect) {
+            Logger().WriteInformation("Dictionary: [");
+            int i = 0;
+            for_each(dict.begin(), dict.end(), [&](string w) {
+                if (i != 0)
+                {
+                    Logger().WriteInformation(" ");
+                }
+                Logger().WriteInformation("%s", w.c_str());
+                i++;
+            });
+            Logger().WriteInformation("]\n");
+            Logger().WriteInformation("    Start:   %s\n", start.c_str());
+            Logger().WriteInformation("    End:     %s\n", end.c_str());
+            int length = Test::String::WordLadder(start, end, dict);
+            int length2 = Test::String::WordLadder2(start, end, dict);
+            int length3 = Test::String::WordLadder2(start, end, dict);
+            vector<vector<string>> ladders = Test::String::WordLadders(start, end, dict);
+            Logger().WriteInformation("    Length:  %d %s %d\n", length, length == expect ? "==" : "!=", expect);
+            Logger().WriteInformation("    Length2: %d %s %d\n", length2, length2 == expect ? "==" : "!=", expect);
+            Logger().WriteInformation("    Length3: %d %s %d\n", length3, length3 == expect ? "==" : "!=", expect);
+            Logger().WriteInformation("    Ladders:\n");
+            for_each(ladders.begin(), ladders.end(), [&](vector<string> &l) {
+                Logger().WriteInformation("            [");
+                int i = 0;
+                for_each(l.begin(), l.end(), [&](string w) {
+                    if (i != 0)
+                    {
+                        Logger().WriteInformation(" ");
+                    }
+                    Logger().WriteInformation("%s", w.c_str());
+                    i++;
+                });
+                Logger().WriteInformation("]\n");
+            });
+            ASSERT1(length == expect);
+            ASSERT1(length2 == expect);
+            ASSERT1(length3 == expect);
+        };
+        {
+            unordered_set<string> dict = {"hot"};
+            check("hit", "cog", dict, 0);
+        }
+        {
+            unordered_set<string> dict = {"hot", "dot", "dog", "lot", "log"};
+            check("hit", "cog", dict, 5);
+        }
+        {
+            unordered_set<string> dict = {"ted", "tex", "red", "tax", "tad", "den", "rex", "pee"};
+            check("red", "tax", dict, 4);
+        }
+    });
+
+    Add("MatchSubsequence", [&]() {
+        auto check = [&](const string &input, const string &pattern, int expect) {
+            Logger().WriteInformation("Input:   %s\n", input.c_str());
+            Logger().WriteInformation("Pattern: %s\n", pattern.c_str());
+            int count = Test::String::MatchSubsequence(input, pattern);
+            int count2 = Test::String::MatchSubsequence2(input, pattern);
+            Logger().WriteInformation("Matches: %d %s %d\n", count, count == expect ? "==" : "!=", expect);
+            Logger().WriteInformation("Matches: %d %s %d\n", count2, count2 == expect ? "==" : "!=", expect);
+            ASSERT1(count == expect);
+            ASSERT1(count2 == expect);
+        };
+        check("r", "r", 1);
+        check("s", "r", 0);
+        check("rr", "r", 2);
+        check("rb", "r", 1);
+        check("br", "r", 1);
+        check("vb", "r", 0);
+        check("rabbbit", "rabbit", 3);
+        check("rrrr", "rr", 6);
+        check("rrrrr", "rr", 10);
+        check("rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr", "rr", 861);
+        check("rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr", "rr", 5151);
+        check("aabdbaabeeadcbbdedacbbeecbabebaeeecaeabaedadcbdbcdaabebdadbbaeabdadeaabbabbecebbebcaddaacccebeaeedababedeacdeaaaeeaecbe", "bddabdcae", 10582116);
+    });
+
     Add("BreakString", [&]() {
         int positions[] = {0, 2, 8, 10, 20};
         Matrix<pair<int, int>> cost(5, 5);
@@ -6481,6 +6580,80 @@ Add("12", [&]() {
     for (int j = 0; j < 10; j++)
     {
         cout << j << "\t(" << seam[j].first << ", " << seam[j].second << ")" << endl;
+    }
+});
+
+Add("Run", [&]() {
+    vector<pair<int, int>> tasks{
+        make_pair(1, 30),
+        make_pair(2, 60),
+        make_pair(3, 40),
+        make_pair(4, 50),
+        make_pair(4, 20),
+        make_pair(4, 70),
+        make_pair(6, 10)};
+
+    for_each(tasks.begin(), tasks.end(), [&](pair<int, int> &t) {
+        Logger().WriteInformation("\t(%d, %d)", t.first, t.second);
+    });
+
+    Logger().WriteInformation("\n");
+
+    vector<pair<int, int>> ontime;
+    vector<pair<int, int>> late;
+    Test::UnitTimeTaskScheduler::Schedule(tasks, ontime, late);
+
+    for_each(ontime.begin(), ontime.end(), [&](pair<int, int> &t) {
+        Logger().WriteInformation("\t(%d, %d)", t.first, t.second);
+    });
+
+    Logger().WriteInformation("\n");
+
+    for_each(late.begin(), late.end(), [&](pair<int, int> &t) {
+        Logger().WriteInformation("\t(%d, %d)", t.first, t.second);
+    });
+
+    Logger().WriteInformation("\n");
+
+    ASSERT1(ontime.size() == 5);
+    ASSERT1(late.size() == 2);
+});
+
+Add("Random", [&]() {
+    for (int i = 0; i < 100; i++)
+    {
+        int count = Test::Random::Next(1, 100);
+        Logger().WriteInformation("Run %d, %d tasks", i, count);
+
+        vector<pair<int, int>> tasks;
+        for (int j = 0; j < count; j++)
+        {
+            tasks.push_back(make_pair(Test::Random::Next(1, count), Test::Random::Next(1, 1000)));
+        }
+
+        vector<pair<int, int>> ontime;
+        vector<pair<int, int>> late;
+        Test::UnitTimeTaskScheduler::Schedule(tasks, ontime, late);
+
+        Logger().WriteInformation(", %d, %d\n", ontime.size(), late.size());
+
+        for (int j = 0; j < (int)ontime.size(); j++)
+        {
+            ASSERT2(
+                j + 1 <= ontime[j].first,
+                Test::String::Format(
+                    "Ontime task with deadline %d and weight %d is scheduled in time slot %d",
+                    ontime[j].first, ontime[j].second, j + 1));
+        }
+
+        for (int j = 0; j < (int)late.size(); j++)
+        {
+            ASSERT2(
+                j + (int)ontime.size() + 1 > late[j].first,
+                Test::String::Format(
+                    "Late task with deadline %d and weight %d is scheduled in time slot %d",
+                    late[j].first, late[j].second, j + ontime.size() + 1));
+        }
     }
 });
 #endif
