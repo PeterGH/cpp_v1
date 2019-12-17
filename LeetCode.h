14d63e041c3bb5b614ce4fcecbacee762790acbd
Peter <peteryzufl@gmail.com>
2019-12-16 22:31:52 -0800

    test

--- LeetCode.h
+++ LeetCode.h
@@ -2022,7 +2022,158 @@ const char *strStr5(const char *input1, const char *input2) {
     return *input2 == '\0' ? input1 : nullptr;
 }
 
+// 29. Divide Two Integers
+// Given two integers dividend and divisor, divide two integers without using
+// multiplication, division and mod operator. Return the quotient after dividing
+// dividend by divisor. The integer division should truncate toward zero.
+// Example 1:
+// Input: dividend = 10, divisor = 3
+// Output: 3
+// Example 2:
+// Input: dividend = 7, divisor = -3
+// Output: -2
+// Note: Both dividend and divisor will be 32-bit signed integers. The divisor
+// will never be 0. Assume we are dealing with an environment which could only
+// store integers within the 32-bit signed integer range: [−2^31,  2^31 − 1].
+// For the purpose of this problem, assume that your function returns 2^31 − 1
+// when the division result overflows.
+
 } // namespace LeetCode
 } // namespace Test
+int divide(int dividend, int divisor) {
+    bool negative = false;
+    if (dividend < 0) {
+        if (divisor > 0) {
+            negative = true;
+            divisor = -divisor;
+        } else {
+            if (dividend == INT_MIN && divisor == -1)
+                return INT_MAX;
+        }
+    } else {
+        if (divisor < 0) {
+            negative = true;
+            divisor = -divisor;
+        }
+    }
 
+    int q = 0;
+    int n = 1;
+    int d = divisor;
+    if (dividend < 0) {
+        while (dividend <= divisor) {
+            if (dividend - d > 0) {
+                d = d >> 1;
+                n = n >> 1;
+            } else if (dividend - d <= d) {
+                d = d << 1; // runtime error: left shift of negative value -1
+                            // when dividing -1 by 1
+                n = n << 1;
+            } else {
+                dividend += d; // BUG, shoud be subtraction
+                q += n;
+            }
+        }
+
+    } else {
+        while (dividend >= divisor) {
+            if (dividend - d < 0) {
+                d = d >> 1;
+                n = n >> 1;
+            } else if (dividend - d >= d) {
+                d = d << 1;
+                n = n << 1;
+            } else {
+                dividend -= d;
+                q += n;
+            }
+        }
+    }
+    return negative ? -q : q;
+}
+int divide2(int dividend, int divisor) {
+    bool negative = false;
+    if (dividend < 0) {
+        if (divisor > 0) {
+            if (dividend == INT_MIN && divisor == 1)
+                return INT_MIN;
+            negative = true;
+            divisor = -divisor;
+        } else {
+            if (dividend == INT_MIN && divisor == -1)
+                return INT_MAX;
+        }
+    } else {
+        if (divisor < 0) {
+            negative = true;
+            divisor = -divisor;
+        }
+    }
+
+    int q = 0;
+    int n = 1;
+    int d = divisor;
+    if (dividend < 0) {
+        while (dividend <= divisor) {
+            if (dividend - d > 0) {
+                d = (d & (0x7FFFFFFF) >> 1) & 0x80000000;
+                n = n >> 1;
+            } else if (dividend - d <= d) {
+                d = d + d;
+                n = n << 1; // runtime error: left shift of negative value
+                            // -2147483648 when divided by -3
+            } else {
+                dividend -= d;
+                q += n;
+            }
+        }
+
+    } else {
+        while (dividend >= divisor) {
+            if (dividend - d < 0) {
+                d = d >> 1;
+                n = n >> 1;
+            } else if (dividend - d >= d) {
+                d = d << 1;
+                n = n << 1;
+            } else {
+                dividend -= d;
+                q += n;
+            }
+        }
+    }
+    return negative ? -q : q;
+}
+int divide3(int dividend, int divisor) {
+    if (dividend == INT_MIN) {
+        if (divisor == -1)
+            return INT_MAX;
+        if (divisor == 1)
+            return INT_MIN;
+        if (divisor == INT_MIN)
+            return 1;
+    }
+    if (divisor == INT_MIN)
+        return 0;
+    bool negative =
+        (dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0);
+    unsigned int udividend = dividend;
+    unsigned int udivisor = divisor; // wrong if divide 7 by -3
+    int q = 0;
+    int n = 1;
+    unsigned int d = udivisor;
+    while (udividend >= udivisor) {
+        if (udividend < d) {
+            d = d >> 1;
+            n = n >> 1;
+        } else if (udividend - d >= d) {
+            d = d << 1;
+            n = n << 1;
+        } else {
+            udividend -= d;
+            q += n;
+        }
+    }
+    return negative ? -q : q;
+}
 #endif
