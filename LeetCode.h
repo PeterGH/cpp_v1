d96a9689d5ee37fa52562a58553e8e4c13e8a6d4
Peter <peteryzufl@gmail.com>
2020-01-15 22:00:03 -0800

    test

--- LeetCode.h
+++ LeetCode.h
@@ -10824,6 +10824,44 @@ int maxProfitII(const vector<int> &prices) {
     return p;
 }
 
+// 123. Best Time to Buy and Sell Stock III
+// Say you have an array for which the ith element is the price of a given stock
+// on day i. Design an algorithm to find the maximum profit. You may complete at
+// most two transactions. Note: You may not engage in multiple transactions at
+// the same time (i.e., you must sell the stock before you buy again). Example
+// 1: Input: [3,3,5,0,0,3,1,4] Output: 6 Explanation: Buy on day 4 (price = 0)
+// and sell on day 6 (price = 3), profit = 3-0 = 3. Then buy on day 7 (price =
+// 1) and sell on day 8 (price = 4), profit = 4-1 = 3. Example 2: Input:
+// [1,2,3,4,5] Output: 4 Explanation: Buy on day 1 (price = 1) and sell on day 5
+// (price = 5), profit = 5-1 = 4. Note that you cannot buy on day 1, buy on day
+// 2 and sell them later, as you are engaging multiple transactions at the same
+// time. You must sell before buying again. Example 3: Input: [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transaction is done, i.e. max profit = 0.
+// Dynamic Programming
+// Let p[k, j] be the profit when k transactions are done at index j
+// Let p[j] = prices[j]
+// p[k, j] = max(p[k, j - 1],
+//               p[k - 1, j - 2] + p[j] - p[j - 1],
+//               p[k - 1, j - 3] + p[j] - p[j - 2],
+//               ......
+//               p[k - 1, 2(k - 1) - 1] + p[j] - p[2(k - 1)])
+//   j 0
+// k 0 p[0,0] p[0,1] p[0,2] ...... p[0,(n-2)] p[0,(n-1)]
+//   1        p[1,1] p[1,2] ...... p[1,(n-2)] p[1,(n-1)]
+//   2                      p[2,3] p[2,4] ...... p[2,(n-2)] p[2,(n-1)]
+// For k = 2
+// p[2, j] = max(p[2, j - 1],
+//               p[1, j - 2] + p[j] - p[j - 1],
+//               p[1, j - 3] + p[j] - p[j - 2],
+//               ......
+//               p[1, 1] + p[j] - p[2])
+// p[1, j] = max(p[1, j - 1],
+//               p[0, j - 2] + p[j] - p[j - 1],
+//               p[0, j - 3] + p[j] - p[j - 2],
+//               ......
+//               p[0, -1] + p[j] - p[0])
+int maxProfitIII(const vector<int> &prices) {}
 } // namespace LeetCode
 } // namespace Test
 #endif
