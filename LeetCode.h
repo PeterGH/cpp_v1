ffbee0eff91e612fc8323b16247dbe4383b86e9c
Peter <peteryzufl@gmail.com>
2020-03-07 13:14:41 -0800

    test

--- LeetCode.h
+++ LeetCode.h
@@ -15652,6 +15652,69 @@ int minSubArrayLen(int s, const vector<int> &nums) {
     }
     return m;
 }
+
+// 210. Course Schedule II
+// There are a total of n courses you have to take, labeled from 0 to n-1. Some
+// courses may have prerequisites, for example to take course 0 you have to
+// first take course 1, which is expressed as a pair: [0,1]. Given the total
+// number of courses and a list of prerequisite pairs, return the ordering of
+// courses you should take to finish all courses. There may be multiple correct
+// orders, you just need to return one of them. If it is impossible to finish
+// all courses, return an empty array. Example 1: Input: 2, [[1,0]] Output:
+// [0,1] Explanation: There are a total of 2 courses to take. To take course 1
+// you should have finished course 0. So the correct course order is [0,1] .
+// Example 2:
+// Input: 4, [[1,0],[2,0],[3,1],[3,2]]
+// Output: [0,1,2,3] or [0,2,1,3]
+// Explanation: There are a total of 4 courses to take. To take course 3 you
+// should have finished both courses 1 and 2. Both courses 1 and 2 should be
+// taken after you finished course 0. So one correct course order is [0,1,2,3].
+// Another correct ordering is [0,2,1,3] . Note: The input prerequisites is a
+// graph represented by a list of edges, not adjacency matrices. Read more about
+// how a graph is represented. You may assume that there are no duplicate edges
+// in the input prerequisites.
+vector<int> findOrder(int numCourses,
+                      const vector<vector<int>> &prerequisites) {
+    map<int, vector<int>> g;
+    for_each(prerequisites.cbegin(), prerequisites.cend(),
+             [&](const vector<int> &e) {
+                 if (g.find(e[0]) == g.end())
+                     g[e[0]] = vector<int>();
+                 g[e[0]].push_back(e[1]);
+             });
+    vector<pair<int, int>> rankNodeVector;
+    set<int> visited;
+    set<int> path;
+    bool solvable = true;
+    int rank = 0;
+    function<bool(int)> solve = [&](int i) -> bool {
+        if (visited.find(i) != visited.end())
+            return true;
+        if (g.find(i) != g.end()) {
+            path.insert(i);
+            for (size_t j = 0; j < g[i].size(); j++) {
+                if (path.find(g[i][j]) != path.end()) {
+                    solvable = false;
+                    return false;
+                }
+                solve(g[i][j]);
+            }
+            path.erase(i);
+        }
+        visited.insert(i);
+        rankNodeVector.push_back(make_pair(rank++, i));
+        return true;
+    };
+    for (int i = 0; i < numCourses && solvable; i++)
+        solve(i);
+    vector<int> result;
+    if (solvable) {
+        // sort(rankNodeVector.begin(), rankNodeVector.end());
+        for_each(rankNodeVector.cbegin(), rankNodeVector.cend(),
+                 [&](const pair<int, int> &p) { result.push_back(p.second); });
+    }
+    return result;
+}
 } // namespace LeetCode
 } // namespace Test
 #endif
