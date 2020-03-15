78f6c1699270b0de87fa06289e6ee811288d7d86
Peter <peteryzufl@gmail.com>
2020-03-15 12:10:50 -0700

    test

--- LeetCode.h
+++ LeetCode.h
@@ -16039,6 +16039,35 @@ int findKthLargest3(const vector<int> &nums, int k) {
         q.pop();
     return q.top();
 }
+
+// 216. Combination Sum III
+// Find all possible combinations of k numbers that add up to a number n, given
+// that only numbers from 1 to 9 can be used and each combination should be a
+// unique set of numbers. Note: All numbers will be positive integers. The
+// solution set must not contain duplicate combinations.
+// Example 1:
+// Input: k = 3, n = 7
+// Output: [[1,2,4]]
+// Example 2:
+// Input: k = 3, n = 9
+// Output: [[1,2,6], [1,3,5], [2,3,4]]
+vector<vector<int>> combinationSum3(int k, int n) {
+    vector<vector<int>> result;
+    vector<int> s;
+    function<void(int, int, int)> solve = [&](int i, int c, int t) {
+        if (c == 0 && t == 0) {
+            result.push_back(s);
+            return;
+        }
+        for (int j = i; j <= 9 && j <= t; j++) {
+            s.push_back(j);
+            solve(j + 1, c - 1, t - j);
+            s.pop_back();
+        }
+    };
+    solve(1, k, n);
+    return result;
+}
 } // namespace LeetCode
 } // namespace Test
 #endif
