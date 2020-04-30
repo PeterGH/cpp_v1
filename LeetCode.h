8dfb761ef8b5aad2be9f7b46b02ecbdfefba2530
Peter <peteryzufl@gmail.com>
2020-04-29 21:47:33 -0700

    test

--- LeetCode.h
+++ LeetCode.h
@@ -20841,6 +20841,148 @@ public:
     }
 };
 
+// Max Consecutive Ones
+// Given a binary array, find the maximum number of consecutive 1s in this array.
+// Example 1:
+// Input: [1,1,0,1,1,1]
+// Output: 3
+// Explanation: The first two digits or the last three digits are consecutive 1s.
+// The maximum number of consecutive 1s is 3. Note: The input array will only contain 0 and 1.
+// The length of input array is a positive integer and will not exceed 10,000
+int findMaxConsecutiveOnes(const vector<int> &nums)
+{
+    int c = 0;
+    int m = 0;
+    for (size_t i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] == 0)
+        {
+            m = max(m, c);
+            c = 0;
+        }
+        else
+        {
+            c++;
+        }
+    }
+    m = max(m, c);
+    return m;
+}
+
+// Find Numbers with Even Number of Digits
+// Given an array nums of integers, return how many of them contain an even number of digits.
+// Example 1:
+// Input: nums = [12,345,2,6,7896]
+// Output: 2
+// Explanation:
+// 12 contains 2 digits (even number of digits).
+// 345 contains 3 digits (odd number of digits).
+// 2 contains 1 digit (odd number of digits).
+// 6 contains 1 digit (odd number of digits).
+// 7896 contains 4 digits (even number of digits).
+// Therefore only 12 and 7896 contain an even number of digits.
+// Example 2:
+// Input: nums = [555,901,482,1771]
+// Output: 1
+// Explanation:
+// Only 1771 contains an even number of digits.
+// Constraints:
+// 1 <= nums.length <= 500
+// 1 <= nums[i] <= 10^5
+int findNumbers(const vector<int> &nums)
+{
+    int c = 0;
+    for (size_t i = 0; i < nums.size(); i++)
+    {
+        int n = nums[i];
+        int d = 0;
+        while (n > 0)
+        {
+            d++;
+            n /= 10;
+        }
+        if ((d & 0x1) == 0)
+            c++;
+    }
+    return c;
+}
+
+// Squares of a Sorted Array
+// Given an array of integers A sorted in non-decreasing order, return an array
+// of the squares of each number, also in sorted non-decreasing order.
+// Example 1:
+// Input: [-4,-1,0,3,10]
+// Output: [0,1,9,16,100]
+// Example 2:
+// Input: [-7,-3,2,3,11]
+// Output: [4,9,9,49,121]
+// Note:
+// 1 <= A.length <= 10000
+// -10000 <= A[i] <= 10000
+// A is sorted in non-decreasing order.
+vector<int> sortedSquares(const vector<int> &A)
+{
+    int i = 0;
+    int n = (int)A.size();
+    int j = n - 1;
+    int k = -1;
+    while (i + 1 < j)
+    {
+        k = i + ((j - i) >> 1);
+        if (A[k] > 0)
+            j = k;
+        else if (A[k] < 0)
+            i = k;
+        else
+            break;
+    }
+    vector<int> r;
+    if (i > j)
+        return r;
+    r.resize(n);
+    int t = 0;
+    if (i == j)
+    {
+        r[t++] = A[i] * A[i];
+        return r;
+    }
+    if (i + 1 < j)
+    {
+        r[t++] = 0;
+        i = k - 1;
+        j = k + 1;
+    }
+    while (0 <= i || j < n)
+    {
+        if (0 <= i && j < n)
+        {
+            int a = A[i] * A[i];
+            int b = A[j] * A[j];
+            if (a <= b)
+            {
+                r[t++] = a;
+                i--;
+            }
+            else
+            {
+                r[t++] = b;
+                j++;
+            }
+        }
+        else if (0 <= i)
+        {
+            r[t++] = A[i] * A[i];
+            i--;
+        }
+        else
+        {
+            r[t++] = A[j] * A[j];
+            j++;
+        }
+    }
+    return r;
+}
+
 } // namespace LeetCode
 } // namespace Test
 #endif
