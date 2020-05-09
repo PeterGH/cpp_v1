adb4a104e1d3e40d98e706b5ae987c32549f0f7d
Peter <peteryzufl@gmail.com>
2020-05-09 12:49:44 -0700

    test

--- LeetCode.h
+++ LeetCode.h
@@ -21948,6 +21948,132 @@ bool isPalindrome(ListNode *head)
     return result;
 }
 
+class MultilevelDoubleLinkedList
+{
+public:
+    struct Node
+    {
+        int val;
+        Node *prev;
+        Node *next;
+        Node *child;
+    };
+
+    // Flatten a Multilevel Doubly Linked List
+    // You are given a doubly linked list which in addition to the next and previous
+    // pointers, it could have a child pointer, which may or may not point to a
+    // separate doubly linked list. These child lists may have one or more children
+    // of their own, and so on, to produce a multilevel data structure, as shown in
+    // the example below.
+    // Flatten the list so that all the nodes appear in a single-level, doubly linked
+    // list. You are given the head of the first level of the list.
+    // Example 1:
+    // Input: head = [1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
+    // Output: [1,2,3,7,8,11,12,9,10,4,5,6]
+    // Explanation:
+    // The multilevel linked list in the input is as follows:
+    // After flattening the multilevel linked list it becomes:
+    // Example 2:
+    // Input: head = [1,2,null,3]
+    // Output: [1,3,2]
+    // Explanation:
+    // The input multilevel linked list is as follows:
+    //   1---2---NULL
+    //   |
+    //   3---NULL
+    // Example 3:
+    // Input: head = []
+    // Output: []
+    // How multilevel linked list is represented in test case:
+    // We use the multilevel linked list from Example 1 above:
+    //  1---2---3---4---5---6--NULL
+    //          |
+    //          7---8---9---10--NULL
+    //              |
+    //              11--12--NULL
+    // The serialization of each level is as follows:
+    // [1,2,3,4,5,6,null]
+    // [7,8,9,10,null]
+    // [11,12,null]
+    // To serialize all levels together we will add nulls in each level to signify no node connects to the upper node of the previous level. The serialization becomes:
+    // [1,2,3,4,5,6,null]
+    // [null,null,7,8,9,10,null]
+    // [null,11,12,null]
+    // Merging the serialization of each level and removing trailing nulls we obtain:
+    // [1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
+    // Constraints:
+    // Number of Nodes will not exceed 1000.
+    // 1 <= Node.val <= 10^5
+    Node *flatten(Node *head)
+    {
+        if (head == nullptr)
+            return nullptr;
+        function<Node *(Node *)> flat = [&](Node *h) -> Node * {
+            Node *t = h;
+            while (h != nullptr)
+            {
+                if (h->child == nullptr)
+                {
+                    t = h;
+                    h = h->next;
+                }
+                else
+                {
+                    Node *ch = h->child;
+                    Node *ct = flat(ch);
+                    ct->next = h->next;
+                    if (h->next != nullptr)
+                        h->next->prev = ct;
+                    h->next = ch;
+                    ch->prev = h;
+                    h->child = nullptr;
+                    t = ct;
+                    h = ct->next;
+                }
+            }
+            return t;
+        };
+        flat(head);
+        return head;
+    }
+    Node *flatten2(Node *head)
+    {
+        stack<Node *> s;
+        Node *n = head;
+        Node *t = head;
+        while (!s.empty() || n != nullptr)
+        {
+            if (n != nullptr)
+            {
+                if (n->child == nullptr)
+                {
+                    t = n;
+                    n = n->next;
+                }
+                else
+                {
+                    s.push(n);
+                    n = n->child;
+                    t = n;
+                }
+            }
+            else
+            {
+                n = s.top();
+                s.pop();
+                t->next = n->next;
+                if (n->next != nullptr)
+                    n->next->prev = t;
+                n->next = n->child;
+                n->child->prev = n;
+                n->child = nullptr;
+                n = t->next;
+            }
+        }
+        return head;
+    }
+};
+
 } // namespace LeetCode
 } // namespace Test
 #endif
