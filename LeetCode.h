24d2afac4287e70709f3c8b1538d71963e37726b
Peter <peteryzufl@gmail.com>
2020-05-30 16:53:52 -0700

    test

--- LeetCode.h
+++ LeetCode.h
@@ -24084,6 +24084,72 @@ namespace Test
             return accumulate(S.cbegin(), S.cend(), 0, [&](int c, char s) { return c + (j.find(s) == j.end() ? 0 : 1); });
         }
 
+        // Top K Frequent Elements
+        // Given a non-empty array of integers, return the k most frequent elements.
+        // Example 1:
+        // Input: nums = [1,1,1,2,2,3], k = 2
+        // Output: [1,2]
+        // Example 2:
+        // Input: nums = [1], k = 1
+        // Output: [1]
+        // Note:
+        // You may assume k is always valid, 1 <= k <= number of unique elements.
+        // Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+        // It's guaranteed that the answer is unique, in other words the set of the top k frequent elements is unique.
+        // You can return the answer in any order.
+        vector<int> topKFrequent(const vector<int> &nums, int k)
+        {
+            map<int, int> m;
+            vector<int> h;
+            function<bool(int, int)> less = [&](int i, int j) -> bool {
+                return m[i] > m[j];
+            };
+            for (int n : nums)
+            {
+                if (m.find(n) == m.end())
+                    m[n] = 1;
+                else
+                    m[n]++;
+            }
+            for (const auto &it : m)
+            {
+                if ((int)h.size() < k)
+                {
+                    h.push_back(it.first);
+                }
+                else if (it.second > m[h[0]])
+                {
+                    pop_heap(h.begin(), h.end(), less);
+                    h[k - 1] = it.first;
+                }
+                push_heap(h.begin(), h.end(), less);
+            }
+            return h;
+        }
+        vector<int> topKFrequent2(vector<int> &nums, int k)
+        {
+            map<int, int> m;
+            vector<int> h;
+            function<bool(int, int)> less = [&](int i, int j) -> bool {
+                return m[i] > m[j];
+            };
+            for (int n : nums)
+            {
+                if (m.find(n) == m.end())
+                {
+                    m[n] = 1;
+                    h.push_back(n);
+                }
+                else
+                {
+                    m[n]++;
+                }
+            }
+            sort(h.begin(), h.end(), less);
+            h.resize(k);
+            return h;
+        }
+
     } // namespace LeetCode
 } // namespace Test
 #endif
