8b2da8264dae8c79078f249dfe20c72b248c1d48
Peter <peteryzufl@gmail.com>
2020-05-30 11:01:26 -0700

    test

--- LeetCode.h
+++ LeetCode.h
@@ -23929,6 +23929,27 @@ namespace Test
             return dup;
         }
 
+        // Jewels and Stones
+        // You're given strings J representing the types of stones that are jewels,
+        // and S representing the stones you have.  Each character in S is a type of
+        // stone you have.  You want to know how many of the stones you have are also jewels.
+        // The letters in J are guaranteed distinct, and all characters in J and S
+        // are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+        // Example 1:
+        // Input: J = "aA", S = "aAAbbbb"
+        // Output: 3
+        // Example 2:
+        // Input: J = "z", S = "ZZ"
+        // Output: 0
+        // Note:
+        // S and J will consist of letters and have length at most 50.
+        // The characters in J are distinct.
+        int numJewelsInStones(const string &J, const string &S)
+        {
+            set<char> j(J.cbegin(), J.cend());
+            return accumulate(S.cbegin(), S.cend(), 0, [&](int c, char s) { return c + (j.find(s) == j.end() ? 0 : 1); });
+        }
+
     } // namespace LeetCode
 } // namespace Test
 #endif
