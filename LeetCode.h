c5db4c7eb0802fbc9ec56a2a5299a3134d9cf226
Peter <peteryzufl@gmail.com>
2020-05-30 13:41:41 -0700

    test

--- LeetCode.h
+++ LeetCode.h
@@ -1903,6 +1903,45 @@ namespace Test
             return o;
         }
 
+        // 4Sum II
+        // Given four lists A, B, C, D of integer values, compute how many
+        // tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+        // To make problem a bit easier, all A, B, C, D have same length of
+        // N where 0 <= N <= 500. All integers are in the range of -2^28 to
+        // 2^28 - 1 and the result is guaranteed to be at most 2^31 - 1.
+        // Example:
+        // Input:
+        // A = [ 1, 2]
+        // B = [-2,-1]
+        // C = [-1, 2]
+        // D = [ 0, 2]
+        // Output:
+        // 2
+        // Explanation:
+        // The two tuples are:
+        // 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+        // 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+        int fourSumCount(vector<int> &A, vector<int> &B, vector<int> &C, vector<int> &D)
+        {
+            multimap<int, pair<int, int>> ab;
+            for (int i = 0; i < (int)A.size(); i++)
+            {
+                for (int j = 0; j < (int)B.size(); j++)
+                {
+                    ab.emplace(A[i] + B[j], make_pair(i, j));
+                }
+            }
+            int c = 0;
+            for (int i = 0; i < (int)C.size(); i++)
+            {
+                for (int j = 0; j < (int)D.size(); j++)
+                {
+                    c += ab.count(-(C[i] + D[j]));
+                }
+            }
+            return c;
+        }
+
         void Print(ListNode *node)
         {
             if (node == nullptr)
