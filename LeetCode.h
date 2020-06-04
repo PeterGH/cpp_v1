f18b1c01f6dd0e4871a6b07ccae3110d9818f2b2
Peter <peteryzufl@gmail.com>
2020-06-03 20:14:34 -0700

    test

--- LeetCode.h
+++ LeetCode.h
@@ -19687,6 +19687,110 @@ namespace Test
             }
         };
 
+        // Replace Words
+        // In English, we have a concept called root, which can be followed by some
+        // other words to form another longer word - let's call this word successor.
+        // For example, the root an, followed by other, which can form another word another.
+        // Now, given a dictionary consisting of many roots and a sentence. You need
+        // to replace all the successor in the sentence with the root forming it. If
+        // a successor has many roots can form it, replace it with the root with the
+        // shortest length. You need to output the sentence after the replacement.
+        // Example 1:
+        // Input: dict = ["cat","bat","rat"], sentence = "the cattle was rattled by the battery"
+        // Output: "the cat was rat by the bat"
+        // Constraints:
+        // The input will only have lower-case letters.
+        // 1 <= dict.length <= 1000
+        // 1 <= dict[i].length <= 100
+        // 1 <= sentence words number <= 1000
+        // 1 <= sentence words length <= 1000
+        string replaceWords(const vector<string> &dict, string sentence)
+        {
+            struct Node
+            {
+                char val;
+                bool complete;
+                map<char, Node *> next;
+                Node(char v, bool c) : val(v), complete(c) {}
+                ~Node()
+                {
+                    for (map<char, Node *>::iterator it = next.begin(); it != next.end(); it++)
+                        delete it->second;
+                    next.clear();
+                }
+            } *root = new Node('\0', true);
+            function<void(const string &)> insert = [&](const string &word) {
+                Node *node = root;
+                size_t i = 0;
+                while (i < word.size())
+                {
+                    if (node->next.find(word[i]) == node->next.end())
+                        break;
+                    node = node->next[word[i++]];
+                }
+                while (i < word.size())
+                {
+                    node->next[word[i]] = new Node(word[i], false);
+                    node = node->next[word[i++]];
+                }
+                node->complete = true;
+            };
+            function<size_t(size_t)> match = [&](size_t i) -> size_t {
+                Node *node = root;
+                size_t j = i;
+                while (j < sentence.size())
+                {
+                    if (node->next.find(sentence[j]) == node->next.end())
+                        break;
+                    node = node->next[sentence[j++]];
+                    if (node->complete)
+                        break;
+                }
+                return node->complete ? j : i;
+            };
+            for (const string &r : dict)
+                insert(r);
+            size_t i = 0;
+            size_t j = 0;
+            while (j < sentence.size())
+            {
+                if (sentence[j] == ' ')
+                {
+                    if (i++ < j++)
+                        sentence[i - 1] = sentence[j - 1];
+                }
+                else
+                {
+                    size_t k = match(j);
+                    if (j < k)
+                    {
+                        if (i < j)
+                        {
+                            while (j < k)
+                                sentence[i++] = sentence[j++];
+                        }
+                        else
+                        {
+                            j = k;
+                            i = j;
+                        }
+                        while (j < sentence.size() && sentence[j] != ' ')
+                            j++;
+                    }
+                    else
+                    {
+                        while (j < sentence.size() && sentence[j] != ' ')
+                        {
+                            if (i++ < j++)
+                                sentence[i - 1] = sentence[j - 1];
+                        }
+                    }
+                }
+            }
+            delete root;
+            return sentence.substr(0, i);
+        }
+
         // 209. Minimum Size Subarray Sum
         // Given an array of n positive integers and a positive integer s, find the
         // minimal length of a contiguous subarray of which the sum >= s. If there
