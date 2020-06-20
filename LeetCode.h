e49661b4d649932e05dc94591eb40df3600be0ce
Peter <peteryzufl@gmail.com>
2020-06-20 11:31:42 -0700

    test

--- LeetCode.h
+++ LeetCode.h
@@ -25021,6 +25021,50 @@ namespace Test
             return m;
         }
 
+        class NaryTree
+        {
+        public:
+            class Node
+            {
+            public:
+                int val;
+                vector<Node *> children;
+
+                Node() {}
+
+                Node(int _val)
+                {
+                    val = _val;
+                }
+
+                Node(int _val, vector<Node *> _children)
+                {
+                    val = _val;
+                    children = _children;
+                }
+            };
+            vector<int> preorder(Node *root)
+            {
+                vector<int> result;
+                if (root == nullptr)
+                    return result;
+                stack<Node *> s;
+                s.push(root);
+                while (!s.empty())
+                {
+                    Node *node = s.top();
+                    s.pop();
+                    result.push_back(node->val);
+                    for (int i = (int)node->children.size() - 1; i >= 0; i--)
+                    {
+                        if (node->children[i] != nullptr)
+                            s.push(node->children[i]);
+                    }
+                }
+                return result;
+            }
+        };
+
     } // namespace LeetCode
 } // namespace Test
 #endif
