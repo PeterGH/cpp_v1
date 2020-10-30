fbc5f81d049059f017cebf680070553ecab9aec8
Peter <peteryzufl@gmail.com>
2020-10-29 20:52:18 -0700

    test

--- LeetCode.h
+++ LeetCode.h
@@ -25,6 +25,23 @@ namespace Test
 {
     namespace LeetCode
     {
+        static void GetMedianIndex(int i, int j, int& lowMedianIndex, int& highMedianIndex)
+        {
+            lowMedianIndex = -1;
+            highMedianIndex = -1;
+            if (i > j)
+                return;
+            if (((j - i) & 0x1) == 0) {
+                // odd numbers [i..j]
+                lowMedianIndex = (i + j) / 2; // = i + ((j - i) / 2)
+                highMedianIndex = lowMedianIndex;
+            } else {
+                // even numbers [i..j]
+                lowMedianIndex = (i + j - 1) / 2; // i + ((j - i - 1) / 2)
+                highMedianIndex = (i + j + 1) / 2; // i + ((j - i + 1) / 2)
+            }
+        }
+
         struct ListNode
         {
             int val;
@@ -26758,6 +26775,36 @@ namespace Test
         //       IFNULL((SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT m,1), NULL)
         //   );
         // END
+
+        // 178. Rank Scores
+        // Write a SQL query to rank scores. If there is a tie between two scores, both should have the same ranking. Note that after a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no "holes" between ranks.
+        // +----+-------+
+        // | Id | Score |
+        // +----+-------+
+        // | 1  | 3.50  |
+        // | 2  | 3.65  |
+        // | 3  | 4.00  |
+        // | 4  | 3.85  |
+        // | 5  | 4.00  |
+        // | 6  | 3.65  |
+        // +----+-------+
+        // For example, given the above Scores table, your query should generate the following report (order by highest score):
+        // +-------+---------+
+        // | score | Rank    |
+        // +-------+---------+
+        // | 4.00  | 1       |
+        // | 4.00  | 1       |
+        // | 3.85  | 2       |
+        // | 3.65  | 3       |
+        // | 3.65  | 3       |
+        // | 3.50  | 4       |
+        // +-------+---------+
+        // Important Note: For MySQL solutions, to escape reserved words used as column names, you can use an apostrophe before and after the keyword. For example `Rank`.
+        // SELECT s.Score, COUNT(t.Score) AS `Rank` FROM
+        // (SELECT DISTINCT Score FROM Scores) AS t, Scores AS s
+        // WHERE s.Score <= t.Score
+        // GROUP BY s.Id, s.Score
+        // ORDER BY s.Score DESC;
 
     } // namespace sql
 } // namespace Test
