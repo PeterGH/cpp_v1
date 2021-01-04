b79d67b97760bb63dc324e32c4a792195b372db5
Peter <peteryzufl@gmail.com>
2021-01-03 19:58:11 -0800

    test

--- LeetCode.h
+++ LeetCode.h
@@ -26806,6 +26806,41 @@ namespace Test
         // GROUP BY s.Id, s.Score
         // ORDER BY s.Score DESC;
 
+// 180. Consecutive Numbers
+// Table: Logs
+// +-------------+---------+
+// | Column Name | Type    |
+// +-------------+---------+
+// | id          | int     |
+// | num         | varchar |
+// +-------------+---------+
+// id is the primary key for this table.
+// Write an SQL query to find all numbers that appear at least three times consecutively.
+// Return the result table in any order.
+// The query result format is in the following example:
+// Logs table:
+// +----+-----+
+// | Id | Num |
+// +----+-----+
+// | 1  | 1   |
+// | 2  | 1   |
+// | 3  | 1   |
+// | 4  | 2   |
+// | 5  | 1   |
+// | 6  | 2   |
+// | 7  | 2   |
+// +----+-----+
+// Result table:
+// +-----------------+
+// | ConsecutiveNums |
+// +-----------------+
+// | 1               |
+// +-----------------+
+// 1 is the only number that appears consecutively for at least three times.
+// select distinct l1.num as ConsecutiveNums from logs l1
+// join logs l2 on l1.id = l2.id - 1
+// join logs l3 on l2.id = l3.id - 1
+// where l1.num = l2.num and l2.num = l3.num;
     } // namespace sql
 } // namespace Test
 
