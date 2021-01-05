9cddcf549e126d26bec87dd3183e8d2f57d97478
Peter <peteryzufl@gmail.com>
2021-01-04 20:02:52 -0800

    test

--- LeetCode.h
+++ LeetCode.h
@@ -26841,6 +26841,29 @@ namespace Test
 // join logs l2 on l1.id = l2.id - 1
 // join logs l3 on l2.id = l3.id - 1
 // where l1.num = l2.num and l2.num = l3.num;
+
+// 181. Employees Earning More Than Their Managers
+// The Employee table holds all employees including their managers.
+// Every employee has an Id, and there is also a column for the manager Id.
+// +----+-------+--------+-----------+
+// | Id | Name  | Salary | ManagerId |
+// +----+-------+--------+-----------+
+// | 1  | Joe   | 70000  | 3         |
+// | 2  | Henry | 80000  | 4         |
+// | 3  | Sam   | 60000  | NULL      |
+// | 4  | Max   | 90000  | NULL      |
+// +----+-------+--------+-----------+
+// Given the Employee table, write a SQL query that finds out employees
+// who earn more than their managers. For the above table, Joe is the only employee
+// who earns more than his manager.
+// +----------+
+// | Employee |
+// +----------+
+// | Joe      |
+// +----------+
+// select e.Name as Employee from Employee e
+// join Employee m on e.ManagerId = m.Id
+// where e.Salary > m.Salary
     } // namespace sql
 } // namespace Test
 
