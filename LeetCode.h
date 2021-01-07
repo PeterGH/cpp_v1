d7ff3397b1e81f8ce959a47656057163cc5445f9
Peter <peteryzufl@gmail.com>
2021-01-06 17:52:19 -0800

    test

--- LeetCode.h
+++ LeetCode.h
@@ -26882,6 +26882,35 @@ namespace Test
 // +---------+
 // Note: All emails are in lowercase.
 // select Email from (select Email, count(Id) as Count from Person group by Email) e where Count > 1;
+
+// 183. Customers Who Never Order
+// Suppose that a website contains two tables, the Customers table and the Orders table.
+// Write a SQL query to find all customers who never order anything.
+// Table: Customers.
+// +----+-------+
+// | Id | Name  |
+// +----+-------+
+// | 1  | Joe   |
+// | 2  | Henry |
+// | 3  | Sam   |
+// | 4  | Max   |
+// +----+-------+
+// Table: Orders.
+// +----+------------+
+// | Id | CustomerId |
+// +----+------------+
+// | 1  | 3          |
+// | 2  | 1          |
+// +----+------------+
+// Using the above tables as example, return the following:
+// +-----------+
+// | Customers |
+// +-----------+
+// | Henry     |
+// | Max       |
+// +-----------+
+// select Name as Customers from Customers where Id not in (select CustomerId from Orders);
+
     } // namespace sql
 } // namespace Test
 
