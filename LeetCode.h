3665ef177fd31cf390a93c86fa8277c137adcee4
Peter <peteryzufl@gmai.com>
2021-01-19 00:21:03 -0800

    test

--- LeetCode.h
+++ LeetCode.h
@@ -23691,6 +23691,35 @@ namespace Test
             return count(root);
         }
 
+        // 223. Rectangle Area
+        // Find the total area covered by two rectilinear rectangles in a 2D plane.
+        // Each rectangle is defined by its bottom left corner and top right corner
+        // as shown in the figure.
+        // Rectangle Area
+        // Example:
+        // Input: A = -3, B = 0, C = 3, D = 4, E = 0, F = -1, G = 9, H = 2
+        // Output: 45
+        // Note:
+        // Assume that the total area is never beyond the maximum possible value of int.
+        int computeArea(int A, int B, int C, int D, int E, int F, int G, int H)
+        {
+            long long lA = A;
+            long long lB = B;
+            long long lC = C;
+            long long lD = D;
+            long long lE = E;
+            long long lF = F;
+            long long lG = G;
+            long long lH = H;
+            long long a = (lC - lA) * (lD - lB) + (lG - lE) * (lH - lF);
+            if (lA < lG && lE < lC && lB < lH && lF < lD)
+            {
+                long long b = (min(lC, lG) - max(lA, lE)) * (min(lH, lD) - max(lF, lB));
+                a -= b;
+            }
+            return a;
+        }
+
         // Max Consecutive Ones
         // Given a binary array, find the maximum number of consecutive 1s in this array.
         // Example 1:
