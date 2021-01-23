031a2e2e2d26c73bd0aaeeeb79f5ca328ffe1cb4
Peter <peteryzufl@gmai.com>
2021-01-22 22:03:37 -0800

    test

--- LeetCode.h
+++ LeetCode.h
@@ -25650,6 +25650,133 @@ namespace Test
             a = op(a, true);
             return a;
         }
+        int calculateII2(string s)
+        {
+            stack<pair<char, long long>> e;
+            long long a = 0;
+            function<long long(long long, bool)> op =
+                [&](long long v, bool doAddSub) -> long long {
+                char o;
+                long long t;
+                if (!e.empty() && (e.top().first == '*' || e.top().first == '/'))
+                {
+                    o = e.top().first;
+                    t = e.top().second;
+                    e.pop();
+                    if (o == '*')
+                    {
+                        v = t * v;
+                    }
+                    else
+                    {
+                        v = t / v;
+                    }
+                }
+                if (doAddSub)
+                {
+                    if (!e.empty())
+                    {
+                        o = e.top().first;
+                        t = e.top().second;
+                        e.pop();
+                        if (o == '+')
+                        {
+                            v = t + v;
+                        }
+                        else
+                        {
+                            v = t - v;
+                        }
+                    }
+                }
+                return v;
+            };
+            for (const char &c : s)
+            {
+                if ('0' <= c && c <= '9')
+                {
+                    a = 10 * a + c - '0';
+                }
+                else if (c == '*' || c == '/' || c == '+' || c == '-')
+                {
+                    a = op(a, (c == '+' || c == '-'));
+                    e.push(make_pair(c, a));
+                    a = 0;
+                }
+            }
+            a = op(a, true);
+            return a;
+        }
+
+        // 228. Summary Ranges
+        // You are given a sorted unique integer array nums.
+        // Return the smallest sorted list of ranges that cover all the numbers
+        // in the array exactly. That is, each element of nums is covered by
+        // exactly one of the ranges, and there is no integer x such that x is
+        // in one of the ranges but not in nums.
+        // Each range [a,b] in the list should be output as:
+        // "a->b" if a != b
+        // "a" if a == b
+        // Example 1:
+        // Input: nums = [0,1,2,4,5,7]
+        // Output: ["0->2","4->5","7"]
+        // Explanation: The ranges are:
+        // [0,2] --> "0->2"
+        // [4,5] --> "4->5"
+        // [7,7] --> "7"
+        // Example 2:
+        // Input: nums = [0,2,3,4,6,8,9]
+        // Output: ["0","2->4","6","8->9"]
+        // Explanation: The ranges are:
+        // [0,0] --> "0"
+        // [2,4] --> "2->4"
+        // [6,6] --> "6"
+        // [8,9] --> "8->9"
+        // Example 3:
+        // Input: nums = []
+        // Output: []
+        // Example 4:
+        // Input: nums = [-1]
+        // Output: ["-1"]
+        // Example 5:
+        // Input: nums = [0]
+        // Output: ["0"]
+        // Constraints:
+        // 0 <= nums.length <= 20
+        // -231 <= nums[i] <= 231 - 1
+        // All the values of nums are unique.
+        // nums is sorted in ascending order.
+        vector<string> summaryRanges(vector<int> &nums)
+        {
+            vector<string> ranges;
+            if (nums.empty())
+                return ranges;
+            function<void(int, int)> output =
+                [&](int x, int y) {
+                    ostringstream r;
+                    r << x;
+                    if (x < y)
+                        r << "->" << y;
+                    ranges.push_back(r.str());
+                };
+            int b = nums[0];
+            int e = b;
+            for (size_t i = 1; i < nums.size(); i++)
+            {
+                if (nums[i] == e + 1)
+                {
+                    e++;
+                }
+                else
+                {
+                    output(b, e);
+                    b = nums[i];
+                    e = b;
+                }
+            }
+            output(b, e);
+            return ranges;
+        }
 
         // Decode String
         // Given an encoded string, return its decoded string.
