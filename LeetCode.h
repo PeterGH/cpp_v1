63052cd2b885e782922914b5f1a30a8c6e66f269
Peter <peteryzufl@gmai.com>
2021-01-25 18:51:42 -0800

    test

--- LeetCode.h
+++ LeetCode.h
@@ -25919,6 +25919,59 @@ namespace Test
             return r;
         }
 
+        // 230. Kth Smallest Element in a BST
+        // Given a binary search tree, write a function kthSmallest to find
+        // the kth smallest element in it.
+        // Example 1:
+        // Input: root = [3,1,4,null,2], k = 1
+        //    3
+        //   / \
+        //  1   4
+        //   \
+        //    2
+        // Output: 1
+        // Example 2:
+        // Input: root = [5,3,6,2,4,null,null,1], k = 3
+        //        5
+        //       / \
+        //      3   6
+        //     / \
+        //    2   4
+        //   /
+        //  1
+        // Output: 3
+        // Follow up:
+        // What if the BST is modified (insert/delete operations) often and
+        // you need to find the kth smallest frequently? How would you optimize
+        // the kthSmallest routine?
+        // Constraints:
+        // The number of elements of the BST is between 1 to 10^4.
+        // You may assume k is always valid, 1 <= k <= BST's total elements.
+        int kthSmallest(TreeNode *root, int k)
+        {
+            stack<TreeNode *> s;
+            int i = 0;
+            TreeNode *n = root;
+            while (!s.empty() || n != nullptr)
+            {
+                if (n != nullptr)
+                {
+                    s.push(n);
+                    n = n->left;
+                }
+                else
+                {
+                    TreeNode *t = s.top();
+                    s.pop();
+                    i++;
+                    if (i == k)
+                        return t->val;
+                    n = t->right;
+                }
+            }
+            throw invalid_argument("k");
+        }
+
         // Decode String
         // Given an encoded string, return its decoded string.
         // The encoding rule is: k[encoded_string], where the encoded_string inside the
