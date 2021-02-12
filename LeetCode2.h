15420d6802f5f735107e10ec15f7cea9746d37b7
Peter <peteryzufl@gmail.com>
2021-02-11 18:15:11 -0800

    test

--- LeetCode2.h
+++ LeetCode2.h
@@ -88,6 +88,58 @@ namespace Test
             return num;
         }
 
+        // 260. Single Number III
+        // Given an integer array nums, in which exactly two elements appear only once and
+        // all the other elements appear exactly twice. Find the two elements that appear only
+        // once. You can return the answer in any order.
+        // Follow up: Your algorithm should run in linear runtime complexity.
+        // Could you implement it using only constant space complexity?
+        // Example 1:
+        // Input: nums = [1,2,1,3,2,5]
+        // Output: [3,5]
+        // Explanation:  [5, 3] is also a valid answer.
+        // Example 2:
+        // Input: nums = [-1,0]
+        // Output: [-1,0]
+        // Example 3:
+        // Input: nums = [0,1]
+        // Output: [1,0]
+        // Constraints:
+        // 2 <= nums.length <= 3 * 10^4
+        // -2^31 <= nums[i] <= 2^31 - 1
+        // Each integer in nums will appear twice, only two integers will appear once.
+        vector<int> singleNumber(vector<int> &nums)
+        {
+            long long x = 0; // must be long long
+            for (const int &n : nums)
+            {
+                x ^= n;
+            }
+            int n1 = 0;
+            int n2 = 0;
+            x = x & (-x);
+            for (const int &n : nums)
+            {
+                if (x & n)
+                    n1 ^= n; // must use xor
+                else
+                    n2 ^= n;
+            }
+            return vector<int>{n1, n2};
+        }
+        vector<int> singleNumber2(vector<int> &nums)
+        {
+            set<int> s;
+            for (const int n : nums)
+            {
+                if (s.find(n) == s.end())
+                    s.insert(n);
+                else
+                    s.erase(n);
+            }
+            return vector<int>(s.cbegin(), s.cend());
+        }
+
     } // namespace LeetCode
 } // namespace Test
 
