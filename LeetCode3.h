878ab9f86b3749ebae0e57f1bf20edc2e95822b5
Peter <peteryzufl@gmail.com>
2021-03-02 17:25:25 -0800

    test

--- LeetCode3.h
+++ LeetCode3.h
@@ -191,6 +191,80 @@ namespace Test
             }
         };
 
+        // 304. Range Sum Query 2D - Immutable
+        // Given a 2D matrix matrix, find the sum of the elements inside the rectangle defined
+        // by its upper left corner (row1, col1) and lower right corner (row2, col2).
+        // Range Sum Query 2D
+        // The above rectangle (with the red border) is defined by (row1, col1) = (2, 1) and
+        // (row2, col2) = (4, 3), which contains sum = 8.
+        // Example:
+        // Given matrix = [
+        //   [3, 0, 1, 4, 2],
+        //   [5, 6, 3, 2, 1],
+        //   [1, 2, 0, 1, 5],
+        //   [4, 1, 0, 1, 7],
+        //   [1, 0, 3, 0, 5]
+        // ]
+        // sumRegion(2, 1, 4, 3) -> 8
+        // sumRegion(1, 1, 2, 2) -> 11
+        // sumRegion(1, 2, 2, 4) -> 12
+        // Note:
+        // You may assume that the matrix does not change.
+        // There are many calls to sumRegion function.
+        // You may assume that row1 <= row2 and col1 <= col2.
+        class NumMatrix
+        {
+        private:
+            vector<vector<int>> acc;
+
+        public:
+            NumMatrix(vector<vector<int>> &matrix)
+            {
+                acc.resize(matrix.size());
+                for (size_t i = 0; i < matrix.size(); i++)
+                {
+                    acc[i].resize(matrix[i].size());
+                    for (size_t j = 0; j < matrix[i].size(); j++)
+                    {
+                        if (i == 0 && j == 0)
+                        {
+                            acc[i][j] = matrix[i][j];
+                        }
+                        else if (i == 0 && j > 0)
+                        {
+                            acc[i][j] = acc[i][j - 1] + matrix[i][j];
+                        }
+                        else if (i > 0 && j == 0)
+                        {
+                            acc[i][j] = acc[i - 1][j] + matrix[i][j];
+                        }
+                        else
+                        {
+                            acc[i][j] = acc[i][j - 1] + acc[i - 1][j] - acc[i - 1][j - 1] + matrix[i][j];
+                        }
+                    }
+                }
+            }
+
+            int sumRegion(int row1, int col1, int row2, int col2)
+            {
+                int s = acc[row2][col2];
+                if (row1 == 0 && col1 > 0)
+                {
+                    s -= acc[row2][col1 - 1];
+                }
+                else if (row1 > 0 && col1 == 0)
+                {
+                    s -= acc[row1 - 1][col2];
+                }
+                else if (row1 > 0 && col1 > 0)
+                {
+                    s = s - acc[row2][col1 - 1] - acc[row1 - 1][col2] + acc[row1 - 1][col1 - 1];
+                }
+                return s;
+            }
+        };
+
     }
 }
 
