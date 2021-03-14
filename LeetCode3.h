45199497e159cc1d9d4a95cb7b8e6bd85f516026
Peter <peteryzufl@gmail.com>
2021-03-13 19:50:53 -0800

    test

--- LeetCode3.h
+++ LeetCode3.h
@@ -989,6 +989,70 @@ namespace Test
             }
         };
 
+        // 309. Best Time to Buy and Sell Stock with Cooldown
+        // You are given an array prices where prices[i] is the price of a given stock on the ith day.
+        // Find the maximum profit you can achieve. You may complete as many transactions as you like
+        // (i.e., buy one and sell one share of the stock multiple times) with the following restrictions:
+        // After you sell your stock, you cannot buy stock on the next day (i.e., cooldown one day).
+        // Note: You may not engage in multiple transactions simultaneously
+        // (i.e., you must sell the stock before you buy again).
+        // Example 1:
+        // Input: prices = [1,2,3,0,2]
+        // Output: 3
+        // Explanation: transactions = [buy, sell, cooldown, buy, sell]
+        // Example 2:
+        // Input: prices = [1]
+        // Output: 0
+        // Constraints:
+        // 1 <= prices.length <= 5000
+        // 0 <= prices[i] <= 1000
+        // Let m[i] be the max profit at day i.
+        // m[i] = max{ m[i-1],
+        //             m[i-3] + p[i] - p[i-1],
+        //             m[i-4] + p[i] - p[i-2],
+        //             ......
+        //             m[1] + p[i] - p[3],
+        //                    p[i] - p[2],
+        //                    p[i] - p[1],
+        //                    p[i] - p[0]
+        //           }
+        //  let s[i] be the state at i
+        //  i  p[i]  s[i]                                           m[i]
+        //  0  p[0]  -p[0]                                          0
+        //  1  p[1]  max{-p[1], -p[0]}                              max{m[0], p[1]-p[0]}
+        //  2  p[2]  max{-p[2], -p[1], -p[0]}                       max{m[1], p[2]-p[1], p[2]-p[0]}
+        //  3  p[3]  max{m[1]-p[3], -p[2], -p[1], -p[0]}            max{m[2], p[3]-p[2], p[3]-p[1], p[3]-p[0]}
+        //  4  p[4]  max{m[2]-p[4], m[1]-p[3], -p[2], -p[1], -p[0]} max{m[3], m[1]+p[4]-p[3], p[4]-p[2], p[4]-p[1], p[4]-p[0]}
+        //  5  p[5]                                                 max{m[4], m[2]+p[5]-p[4], m[1]+p[5]-p[3], p[5]-p[2], p[5]-p[1], p[5]-p[0]}
+        int maxProfit(vector<int> &prices)
+        {
+            int m = 0;   // m[i]
+            int m_1 = 0; // m[i-1]
+            int m_2 = 0; // m[i-2]
+            int s = 0;
+            for (int i = 0; i < (int)prices.size(); i++)
+            {
+                if (i == 0)
+                {
+                    m = 0;
+                    s = -prices[0];
+                }
+                else if (i < 3)
+                {
+                    m = max(m_1, prices[i] + s);
+                    s = max(-prices[i], s);
+                }
+                else // i>= 3
+                {
+                    m = max(m_1, prices[i] + s);
+                    s = max(m_2 - prices[i], s);
+                }
+                m_2 = m_1;
+                m_1 = m;
+            }
+            return m;
+        }
+
     }
 }
 
