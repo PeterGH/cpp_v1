923d22cf41de6b018165fe09fc393d7232a3df51
Peter <peteryzufl@gmail.com>
2021-04-10 15:40:21 -0700

    test

--- LeetCode0.h
+++ LeetCode0.h
@@ -1459,6 +1459,551 @@ namespace Test
             return negative ? -r : r;
         }
 
+        // 9. Palindrome Number
+        // Determine whether an integer is a palindrome. An integer is a palindrome
+        // when it reads the same backward as forward.
+        // Example 1:
+        // Input: 121
+        // Output: true
+        // Example 2:
+        // Input: -121
+        // Output: false
+        // Explanation: From left to right, it reads -121. From right to left,
+        // it becomes 121-. Therefore it is not a palindrome.
+        // Example 3:
+        // Input: 10
+        // Output: false
+        // Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+        // Follow up:
+        // Coud you solve it without converting the integer to a string?
+        bool isPalindrome(int x)
+        {
+            if (x < 0)
+                return false;
+            int d = 1;
+            // d is int, so 10 * d will overflow if d is too large,
+            // e.g. when x = 1000000001
+            // while (10 * d <= x)
+            //     d *= 10;
+            int y = x;
+            while (y >= 10)
+            {
+                d *= 10;
+                y /= 10;
+            }
+            while (x > 0)
+            {
+                int l = x % 10;
+                int h = x / d;
+                if (h != l)
+                    return false;
+                x = (x % d) / 10;
+                d /= 100;
+            }
+            return true;
+        }
+        bool isPalindrome2(int x)
+        {
+            if (x < 0)
+                return false;
+            long long y = x;
+            long long d = 1; // use long long to avoid overflow
+            while (y >= 10 * d)
+                d *= 10;
+            int l = 0;
+            int r = 0;
+            while (y > 0)
+            {
+                l = y >= d ? (y / d) : 0; // what if just l = y / d ?
+                r = y % 10;
+                if (l != r)
+                    return false;
+                y = (y % d) / 10;
+                d /= 100;
+            }
+            return true;
+        }
+        bool isPalindrome3(unsigned int number)
+        {
+            if (number < 10)
+                return true;
+            unsigned int base = 1;
+            while ((number / base) >= 10)
+                base *= 10;
+            // Compare the most significant digit (MSD) and
+            // the least significant digit (LSD)
+            while (number >= 10)
+            {
+                if ((number / base) != (number % 10))
+                {
+                    // MSD is different from LSD
+                    return false;
+                }
+                // Peel off MSD and LSD
+                number = (number % base) / 10;
+                base /= 100;
+            }
+            return true;
+        }
+
+        // 11. Container With Most Water
+        // Given n non-negative integers a1, a2, ..., an , where each represents a point
+        // at coordinate (i, ai). n vertical lines are drawn such that the two endpoints
+        // of line i is at (i, ai) and (i, 0). Find two lines, which together with
+        // x-axis forms a container, such that the container contains the most water.
+        // Note: You may not slant the container and n is at least 2.
+        // Example:
+        // Input: [1,8,6,2,5,4,8,3,7]
+        // Output: 49 (between a[1] and a[8]), note that a[6] does not divide the container
+        // formed by a[1] and a[8].
+        int maxArea(const vector<int> &height)
+        {
+            size_t i = 0;
+            size_t j = height.size() - 1;
+            int m = 0;
+            while (i < j)
+            {
+                int a = 0;
+                if (height[i] <= height[j])
+                {
+                    // Increasing i is the only way we may find a larger container.
+                    // Cannot decrease j, because container [h[i], h[j-1]] is guaranteed
+                    // to be smaller.
+                    a = height[i] * (j - i);
+                    i++;
+                }
+                else
+                {
+                    a = height[j] * (j - i);
+                    j--;
+                }
+                m = std::max(m, a);
+            }
+            return m;
+        }
+        int maxArea2(const vector<int> &height)
+        {
+            int n = height.size();
+            if (n <= 1)
+                return 0;
+            vector<int> begin = {0};
+            for (int i = 1; i < n; i++)
+            {
+                if (height[i] > height[begin.back()])
+                    begin.push_back(i);
+            }
+            // begin contains increasing elements
+            vector<int> end = {n - 1};
+            for (int i = n - 2; i >= 0; i--)
+            {
+                if (height[i] > height[end.front()])
+                    end.insert(end.begin(), i);
+            }
+            // end contains decreasing elements
+            int a = 0;
+            for (int j = 0; j < (int)end.size(); j++)
+            {
+                int i = 0;
+                while (i < (int)begin.size() && begin[i] < end[j])
+                {
+                    int b = (end[j] - begin[i]) * min(height[begin[i]], height[end[j]]);
+                    if (b > a)
+                        a = b;
+                    i++;
+                }
+            }
+            return a;
+        }
+        int maxArea3(const vector<int> &height)
+        {
+            int m = 0;
+            for (int i = 0; i < (int)height.size() - 1; i++)
+            {
+                for (int j = i + 1; j < (int)height.size(); j++)
+                {
+                    m = std::max(m, (j - i) * std::min(height[i], height[j]));
+                }
+            }
+            return m;
+        }
+
+        // 12. Integer to Roman
+        // Roman numerals are represented by seven different symbols: I, V, X, L, C, D
+        // and M. (Symbol,Value) = (I,1), (V,5), (X,10), (L,50), (C,100), (D,500),
+        // (M,1000). For example, two is written as II in Roman numeral, just two one's
+        // added together. Twelve is written as, XII, which is simply X + II. The number
+        // twenty seven is written as XXVII, which is XX + V + II. Roman numerals are
+        // usually written largest to smallest from left to right. However, the numeral
+        // for four is not IIII. Instead, the number four is written as IV. Because the
+        // one is before the five we subtract it making four. The same principle applies
+        // to the number nine, which is written as IX. There are six instances where
+        // subtraction is used:
+        //     I can be placed before V (5) and X (10) to make 4 and 9.
+        //     X can be placed before L (50) and C (100) to make 40 and 90.
+        //     C can be placed before D (500) and M (1000) to make 400 and 900.
+        // Given an integer, convert it to a roman numeral. Input is guaranteed to be
+        // within the range from 1 to 3999. Example 1: Input: 3 Output: "III" Example 2:
+        // Input: 4
+        // Output: "IV"
+        // Example 3:
+        // Input: 9
+        // Output: "IX"
+        // Example 4:
+        // Input: 58
+        // Output: "LVIII"
+        // Explanation: L = 50, V = 5, III = 3.
+        // Example 5:
+        // Input: 1994
+        // Output: "MCMXCIV"
+        // Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+        string intToRoman(int num)
+        {
+            string r;
+            if (num >= 1000)
+            {
+                r.append(num / 1000, 'M');
+                num %= 1000;
+            }
+            if (num >= 900)
+            {
+                r.append("CM");
+                num -= 900;
+            }
+            if (num >= 500)
+            {
+                r.append(1, 'D');
+                num -= 500;
+            }
+            if (num >= 400)
+            {
+                r.append("CD");
+                num -= 400;
+            }
+            if (num >= 100)
+            {
+                r.append(num / 100, 'C');
+                num %= 100;
+            }
+            if (num >= 90)
+            {
+                r.append("XC");
+                num -= 90;
+            }
+            if (num >= 50)
+            {
+                r.append(1, 'L');
+                num -= 50;
+            }
+            if (num >= 40)
+            {
+                r.append("XL");
+                num -= 40;
+            }
+            if (num >= 10)
+            {
+                r.append(num / 10, 'X');
+                num %= 10;
+            }
+            if (num == 9)
+            {
+                r.append("IX");
+                num -= 9;
+            }
+            if (num >= 5)
+            {
+                r.append(1, 'V');
+                num -= 5;
+            }
+            if (num == 4)
+            {
+                r.append("IV");
+                num -= 4;
+            }
+            if (num >= 1)
+            {
+                r.append(num, 'I');
+            }
+            return r;
+        }
+        string intToRoman2(int num)
+        {
+            string s;
+            if (num <= 0)
+                return s;
+            int d = num / 1000;
+            if (d > 0)
+                s.append(d, 'M');
+            num = num % 1000;
+            if (num == 0)
+                return s;
+            if (num >= 900)
+            {
+                s.append(1, 'C');
+                s.append(1, 'M');
+                num -= 900;
+            }
+            else if (num >= 500)
+            {
+                s.append(1, 'D');
+                num -= 500;
+                if (num == 0)
+                    return s;
+                d = num / 100;
+                if (d > 0)
+                    s.append(d, 'C');
+                num = num % 100;
+            }
+            else if (num >= 400)
+            {
+                s.append(1, 'C');
+                s.append(1, 'D');
+                num -= 400;
+            }
+            else
+            {
+                d = num / 100;
+                if (d > 0)
+                    s.append(d, 'C');
+                num = num % 100;
+            }
+            // Now 0 <= num < 100
+            if (num == 0)
+                return s;
+            if (num >= 90)
+            {
+                s.append(1, 'X');
+                s.append(1, 'C');
+                num -= 90;
+            }
+            else if (num >= 50)
+            {
+                s.append(1, 'L');
+                num -= 50;
+                if (num == 0)
+                    return s;
+                d = num / 10;
+                if (d > 0)
+                    s.append(d, 'X');
+                num = num % 10;
+            }
+            else if (num >= 40)
+            {
+                s.append(1, 'X');
+                s.append(1, 'L');
+                num -= 40;
+            }
+            else
+            {
+                d = num / 10;
+                if (d > 0)
+                    s.append(d, 'X');
+                num = num % 10;
+            }
+            // Now 0 <= num < 10
+            if (num == 0)
+                return s;
+            if (num == 9)
+            {
+                s.append(1, 'I');
+                s.append(1, 'X');
+            }
+            else if (num >= 5)
+            {
+                s.append(1, 'V');
+                num -= 5;
+                s.append(num, 'I');
+            }
+            else if (num == 4)
+            {
+                s.append(1, 'I');
+                s.append(1, 'V');
+            }
+            else
+            {
+                s.append(num, 'I');
+            }
+            return s;
+        }
+
+        // 13. Roman to Integer
+        // Roman numerals are represented by seven different symbols: I, V, X, L, C, D
+        // and M. (Symbol,Value) = (I,1), (V,5), (X,10), (L,50), (C,100), (D,500), For
+        // example, two is written as II in Roman numeral, just two one's added
+        // together. Twelve is written as, XII, which is simply X + II. The number
+        // twenty seven is written as XXVII, which is XX + V + II. Roman numerals are
+        // usually written largest to smallest from left to right. However, the numeral
+        // for four is not IIII. Instead, the number four is written as IV. Because the
+        // one is before the five we subtract it making four. The same principle applies
+        // to the number nine, which is written as IX. There are six instances where
+        // subtraction is used:
+        //     I can be placed before V (5) and X (10) to make 4 and 9.
+        //     X can be placed before L (50) and C (100) to make 40 and 90.
+        //     C can be placed before D (500) and M (1000) to make 400 and 900.
+        // Given a roman numeral, convert it to an integer. Input is guaranteed to be
+        // within the range from 1 to 3999. Example 1: Input: "III" Output: 3 Example 2:
+        // Input: "IV"
+        // Output: 4
+        // Example 3:
+        // Input: "IX"
+        // Output: 9
+        // Example 4:
+        // Input: "LVIII"
+        // Output: 58
+        // Explanation: L = 50, V= 5, III = 3.
+        // Example 5:
+        // Input: "MCMXCIV"
+        // Output: 1994
+        // Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+        int romanToInt(const string &s)
+        {
+            int r = 0;
+            size_t i = 0;
+            while (i < s.size())
+            {
+                switch (s[i])
+                {
+                case 'I':
+                    if (i + 1 < s.size() && s[i + 1] == 'V')
+                    {
+                        r += 4;
+                        i += 2;
+                    }
+                    else if (i + 1 < s.size() && s[i + 1] == 'X')
+                    {
+                        r += 9;
+                        i += 2;
+                    }
+                    else
+                    {
+                        r += 1;
+                        i++;
+                    }
+                    break;
+                case 'V':
+                    r += 5;
+                    i++;
+                    break;
+                case 'X':
+                    if (i + 1 < s.size() && s[i + 1] == 'L')
+                    {
+                        r += 40;
+                        i += 2;
+                    }
+                    else if (i + 1 < s.size() && s[i + 1] == 'C')
+                    {
+                        r += 90;
+                        i += 2;
+                    }
+                    else
+                    {
+                        r += 10;
+                        i++;
+                    }
+                    break;
+                case 'L':
+                    r += 50;
+                    i++;
+                    break;
+                case 'C':
+                    if (i + 1 < s.size() && s[i + 1] == 'D')
+                    {
+                        r += 400;
+                        i += 2;
+                    }
+                    else if (i + 1 < s.size() && s[i + 1] == 'M')
+                    {
+                        r += 900;
+                        i += 2;
+                    }
+                    else
+                    {
+                        r += 100;
+                        i++;
+                    }
+                    break;
+                case 'D':
+                    r += 500;
+                    i++;
+                    break;
+                case 'M':
+                    r += 1000;
+                    i++;
+                    break;
+                default:
+                    throw invalid_argument(s);
+                }
+            }
+            return r;
+        }
+
+        // 14. Longest Common Prefix
+        // Write a function to find the longest common prefix string amongst an array of
+        // strings. If there is no common prefix, return an empty string "". Example 1:
+        // Input: ["flower","flow","flight"]
+        // Output: "fl"
+        // Example 2:
+        // Input: ["dog","racecar","car"]
+        // Output: ""
+        // Explanation: There is no common prefix among the input strings.
+        // Note: All given inputs are in lowercase letters a-z.
+        string longestCommonPrefix(const vector<string> &strs)
+        {
+            if (strs.empty())
+                return "";
+            if (strs.size() == 1)
+                return strs[0];
+            size_t i = 0;
+            while (i < strs[0].size())
+            {
+                for (size_t j = 1; j < strs.size(); j++)
+                {
+                    if (i == strs[j].size() || strs[0][i] != strs[j][i])
+                        return strs[0].substr(0, i);
+                }
+                i++;
+            }
+            return strs[0];
+        }
+        string longestCommonPrefix2(const vector<string> &strs)
+        {
+            if (strs.empty())
+                return "";
+            if (strs.size() == 1)
+                return strs[0];
+            function<size_t(size_t, size_t)> prefix = [&](size_t l,
+                                                          size_t i) -> size_t {
+                size_t j = 0;
+                while (j < l && j < strs[i].size() && strs[0][j] == strs[i][j])
+                    j++;
+                return j;
+            };
+            size_t k = strs[0].size();
+            for (size_t i = 1; i < strs.size(); i++)
+            {
+                k = prefix(k, i);
+                if (k == 0)
+                    break;
+            }
+            return strs[0].substr(0, k);
+        }
+        string longestCommonPrefix3(const vector<string> &strs)
+        {
+            if (strs.empty())
+                return "";
+            int l = strs[0].size();
+            for (int i = 1; i < (int)strs.size() && l > 0; i++)
+            {
+                int j = 0;
+                for (j = 0; j < l && j < (int)strs[i].size(); j++)
+                {
+                    if (strs[0][j] != strs[i][j])
+                        break;
+                }
+                l = min(l, j);
+            }
+            return l > 0 ? strs[0].substr(0, l) : "";
+        }
+
     } // namespace LeetCode
 } // namespace Test
 
