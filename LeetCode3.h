6a2e8849dde7366c85da27ee56bd23e154960b33
Peter <peteryzufl@gmail.com>
2021-04-10 12:17:29 -0700

    test

--- LeetCode3.h
+++ LeetCode3.h
@@ -1502,6 +1502,45 @@ namespace Test
             return counts;
         }
 
+        // 316. Remove Duplicate Letters
+        // Given a string s, remove duplicate letters so that every letter appears once and only once.
+        // You must make sure your result is the smallest in lexicographical order among all possible results.
+        // Note: This question is the same as 1081: https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
+        // Example 1:
+        // Input: s = "bcabc"
+        // Output: "abc"
+        // Example 2:
+        // Input: s = "cbacdcbc"
+        // Output: "acdb"
+        // Constraints:
+        // 1 <= s.length <= 10^4
+        // s consists of lowercase English letters.
+        string removeDuplicateLetters(string s)
+        {
+            bitset<26> valid;
+            vector<int> count(26, 0);
+            for (const char c : s)
+            {
+                count[c - 'a']++;
+            }
+            string r;
+            for (const char c : s)
+            {
+                if (!valid.test(c - 'a'))
+                {
+                    while (!r.empty() && r.back() > c && count[r.back() - 'a'] > 0)
+                    {
+                        valid.reset(r.back() - 'a');
+                        r.pop_back();
+                    }
+                    valid.set(c - 'a');
+                    r.append(1, c);
+                }
+                count[c - 'a']--;
+            }
+            return r;
+        }
+
     }
 }
 
