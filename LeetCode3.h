2459b4537ebf0d7f48ab08544323884760651602
Peter <peteryzufl@gmail.com>
2021-04-22 21:29:10 -0700

    test

--- LeetCode3.h
+++ LeetCode3.h
@@ -1920,6 +1920,72 @@ namespace Test
             return count[amount] > amount ? -1 : count[amount];
         }
 
+        // 324. Wiggle Sort II
+        // Given an integer array nums, reorder it such that nums[0] < nums[1] > nums[2] < nums[3] ...
+        // You may assume the input array always has a valid answer.
+        // Example 1:
+        // Input: nums = [1,5,1,1,6,4]
+        // Output: [1,6,1,5,1,4]
+        // Explanation: [1,4,1,5,1,6] is also accepted.
+        // Example 2:
+        // Input: nums = [1,3,2,2,3,1]
+        // Output: [2,3,1,3,1,2]
+        // Constraints:
+        // 1 <= nums.length <= 5 * 10^4
+        // 0 <= nums[i] <= 5000
+        // It is guaranteed that there will be an answer for the given input nums.
+        // Follow Up: Can you do it in O(n) time and/or in-place with O(1) extra space?
+        void wiggleSort(vector<int> &nums)
+        {
+            vector<int> v(nums.begin(), nums.end());
+            sort(v.begin(), v.end());
+            int n = nums.size();
+            int largeCount = n >> 1;
+            int smallCount = n - largeCount;
+            int i = 1;
+            n--;
+            while (largeCount > 0)
+            {
+                nums[i] = v[n--];
+                i += 2;
+                largeCount--;
+            }
+            i = 0;
+            while (smallCount > 0)
+            {
+                nums[i] = v[n--];
+                i += 2;
+                smallCount--;
+            }
+        }
+        void wiggleSort2(vector<int> &nums)
+        {
+            int n = nums.size();
+            int m = (n - 1) >> 1;
+            // Rearrange so that nums[m] is the m-th element
+            nth_element(nums.begin(), nums.begin() + m, nums.end());
+            int median = nums[m];
+            int o = 1; // odd position
+            int e = ((n & 1) == 0) ? n - 2 : n - 1; // even position
+            for (int i = 0; i < n; i++)
+            {
+                if (nums[i] > median)
+                {
+                    if ((i & 1) && i < o) // i is odd and already processed
+                        continue;
+                    swap(nums[o], nums[i--]);
+                    o += 2;
+                }
+                else if (nums[i] < median)
+                {
+                    if (!(i & 1) && i > e) // i is even and already processed
+                        continue;
+                    swap(nums[e], nums[i--]);
+                    e -= 2;
+                }
+            }
+        }
+
     }
 }
 
