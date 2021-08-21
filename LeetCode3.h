9270b3943bcd151982a2562ab8f0c632112a23f1
Peter <peteryzufl@gmail.com>
2021-08-21 11:04:26 -0700

    test

--- LeetCode3.h
+++ LeetCode3.h
@@ -3453,6 +3453,48 @@ namespace Test
             return output;
         }
 
+        // 337. House Robber III
+        // The thief has found himself a new place for his thievery again. There is only
+        // one entrance to this area, called root. Besides the root, each house has one
+        // and only one parent house. After a tour, the smart thief realized that all houses
+        // in this place form a binary tree. It will automatically contact the police if
+        // two directly-linked houses were broken into on the same night. Given the root
+        // of the binary tree, return the maximum amount of money the thief can rob without
+        // alerting the police.
+        // Example 1:
+        // Input: root = [3,2,3,null,3,null,1]
+        // Output: 7
+        // Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+        // Example 2:
+        // Input: root = [3,4,5,1,3,null,1]
+        // Output: 9
+        // Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+        // Constraints:
+        // The number of nodes in the tree is in the range [1, 10^4].
+        // 0 <= Node.val <= 10^4
+        int rob(TreeNode *root)
+        {
+            function<int(TreeNode *, bool)>
+                solve = [&](TreeNode *n, bool s) -> int
+            {
+                if (n == nullptr)
+                    return 0;
+                if (s)
+                {
+                    return n->val + solve(n->left, false) + solve(n->right, false);
+                }
+                else
+                {
+                    int m = solve(n->left, false) + solve(n->right, false);
+                    m = max(m, solve(n->left, true) + solve(n->right, false));
+                    m = max(m, solve(n->left, false) + solve(n->right, true));
+                    m = max(m, solve(n->left, true) + solve(n->right, true));
+                    return m;
+                }
+            };
+            return max(solve(root, false), solve(root, true));
+        }
+
         // 349. Intersection of Two Arrays
         // Given two arrays, write a function to compute their intersection.
         // Example 1:
