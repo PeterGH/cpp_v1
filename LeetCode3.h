0980f70cb774c02ef70e2ccf274515f40b9aa4df
Peter <peteryzufl@gmail.com>
2021-09-04 12:17:01 -0700

    test

--- LeetCode3.h
+++ LeetCode3.h
@@ -3697,6 +3697,117 @@ namespace Test
             return output;
         }
 
+        // 341. Flatten Nested List Iterator
+        // You are given a nested list of integers nestedList. Each element is either
+        // an integer or a list whose elements may also be integers or other lists.
+        // Implement an iterator to flatten it.
+        // Implement the NestedIterator class:
+        // NestedIterator(List<NestedInteger> nestedList) Initializes the iterator with
+        // the nested list nestedList.
+        // int next() Returns the next integer in the nested list.
+        // boolean hasNext() Returns true if there are still some integers in the nested
+        // list and false otherwise.
+        // Your code will be tested with the following pseudocode:
+        // initialize iterator with nestedList
+        // res = []
+        // while iterator.hasNext()
+        //     append iterator.next() to the end of res
+        // return res
+        // If res matches the expected flattened list, then your code will be judged as correct.
+        // Example 1:
+        // Input: nestedList = [[1,1],2,[1,1]]
+        // Output: [1,1,2,1,1]
+        // Explanation: By calling next repeatedly until hasNext returns false, the order
+        // of elements returned by next should be: [1,1,2,1,1].
+        // Example 2:
+        // Input: nestedList = [1,[4,[6]]]
+        // Output: [1,4,6]
+        // Explanation: By calling next repeatedly until hasNext returns false, the order
+        // of elements returned by next should be: [1,4,6].
+        // Constraints:
+        // 1 <= nestedList.length <= 500
+        // The values of the integers in the nested list is in the range [-10^6, 10^6].
+        class NestedListIterator
+        {
+        public:
+            class NestedInteger
+            {
+            private:
+                int _v;
+                vector<NestedInteger> _list;
+
+            public:
+                NestedInteger(int v) : _v(v)
+                {
+                }
+
+                NestedInteger(const vector<NestedInteger> &list) : _list(list)
+                {
+                }
+
+                // Return true if this NestedInteger holds a single integer, rather than a nested list.
+                bool isInteger() const
+                {
+                    return _list.empty();
+                }
+
+                // Return the single integer that this NestedInteger holds, if it holds a single integer
+                // The result is undefined if this NestedInteger holds a nested list
+                int getInteger() const { return _v; }
+
+                // Return the nested list that this NestedInteger holds, if it holds a nested list
+                // The result is undefined if this NestedInteger holds a single integer
+                const vector<NestedInteger> &getList() const { return _list; }
+            };
+
+            class NestedIterator
+            {
+            private:
+                stack<pair<vector<NestedInteger>::iterator, vector<NestedInteger>::iterator>> _s;
+
+            public:
+                NestedIterator(vector<NestedInteger> &nestedList)
+                {
+                    _s.push(make_pair(nestedList.begin(), nestedList.end()));
+                }
+
+                int next()
+                {
+                    while (!_s.top().first->isInteger())
+                    {
+                        pair<vector<NestedInteger>::iterator, vector<NestedInteger>::iterator> p = make_pair(_s.top().first->getList().begin(), _s.top().first->getList().end());
+                        _s.top().first++;
+                        _s.push(p);
+                    }
+                    int v = _s.top().first->getInteger();
+                    _s.top().first++;
+                    return v;
+                }
+
+                bool hasNext()
+                {
+                    while (!_s.empty())
+                    {
+                        if (_s.top().first == _s.top().second)
+                        {
+                            _s.pop();
+                        }
+                        else if (_s.top().first->isInteger())
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            pair<vector<NestedInteger>::iterator, vector<NestedInteger>::iterator> p = make_pair(_s.top().first->getList().begin(), _s.top().first->getList().end());
+                            _s.top().first++;
+                            _s.push(p);
+                        }
+                    }
+                    return false;
+                }
+            };
+        };
+
         // 349. Intersection of Two Arrays
         // Given two arrays, write a function to compute their intersection.
         // Example 1:
--- LeetCodeTest3.h
+++ LeetCodeTest3.h
@@ -5,121 +5,165 @@
 
 void LeetCodeTest::Init3(void)
 {
-    Add("300. Longest Increasing Subsequence", [&]() {
-        auto check = [&](const vector<int> &v, int e) {
-            int l = lengthOfLIS(v);
-            int l2 = lengthOfLIS2(v);
-            int l3 = lengthOfLIS3(v);
-            int l4 = lengthOfLIS4(v);
-            int l5 = lengthOfLIS5(v);
-            ASSERT1(l == e);
-            ASSERT1(l2 == e);
-            ASSERT1(l3 == e);
-            ASSERT1(l4 == e);
-            ASSERT1(l5 == e);
-        };
-        check({10, 9, 2, 5, 3, 7, 101, 18}, 4);
-    });
-
-    Add("306. Additive Number", [&]() {
-        auto check = [&](const string &n, bool e) {
-            bool r = isAdditiveNumber(n);
-            ASSERT1(r == e);
-        };
-        check("199100199", true);
-        check("11111111111011111111111", true);
-    });
-
-    Add("307. Range Sum Query - Mutable", [&]() {
+    Add("300. Longest Increasing Subsequence", [&]()
         {
-            NumArrayMutable6 a6({1, 3, 5});
-            int v = a6.sumRange(0, 2);
-            Logger() << "sumRange(0, 2) = " << v << endl;
-            Logger() << "update(1, 2)" << endl;
-            a6.update(1, 2);
-            v = a6.sumRange(0, 2);
-            Logger() << "sumRange(0, 2) = " << v << endl;
-        }
+            auto check = [&](const vector<int> &v, int e)
+            {
+                int l = lengthOfLIS(v);
+                int l2 = lengthOfLIS2(v);
+                int l3 = lengthOfLIS3(v);
+                int l4 = lengthOfLIS4(v);
+                int l5 = lengthOfLIS5(v);
+                ASSERT1(l == e);
+                ASSERT1(l2 == e);
+                ASSERT1(l3 == e);
+                ASSERT1(l4 == e);
+                ASSERT1(l5 == e);
+            };
+            check({10, 9, 2, 5, 3, 7, 101, 18}, 4);
+        });
+
+    Add("306. Additive Number", [&]()
         {
-            NumArrayMutable6 a6({0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
-            a6.print();
-        }
+            auto check = [&](const string &n, bool e)
+            {
+                bool r = isAdditiveNumber(n);
+                ASSERT1(r == e);
+            };
+            check("199100199", true);
+            check("11111111111011111111111", true);
+        });
+
+    Add("307. Range Sum Query - Mutable", [&]()
         {
-            NumArrayMutable6 a6({-28, -39, 53, 65, 11, -56, -65, -39, -43, 97});
-            a6.sumRange(5, 6);
-            a6.update(9, 27);
-            a6.sumRange(2, 3);
-            a6.sumRange(6, 7);
-            a6.update(1, -82);
-            a6.update(3, -72);
-            a6.sumRange(3, 7);
-            a6.sumRange(1, 8);
-            a6.update(5, 13);
-            a6.update(4, -67);
-        }
-    });
+            {
+                NumArrayMutable6 a6({1, 3, 5});
+                int v = a6.sumRange(0, 2);
+                Logger() << "sumRange(0, 2) = " << v << endl;
+                Logger() << "update(1, 2)" << endl;
+                a6.update(1, 2);
+                v = a6.sumRange(0, 2);
+                Logger() << "sumRange(0, 2) = " << v << endl;
+            }
+            {
+                NumArrayMutable6 a6({0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+                a6.print();
+            }
+            {
+                NumArrayMutable6 a6({-28, -39, 53, 65, 11, -56, -65, -39, -43, 97});
+                a6.sumRange(5, 6);
+                a6.update(9, 27);
+                a6.sumRange(2, 3);
+                a6.sumRange(6, 7);
+                a6.update(1, -82);
+                a6.update(3, -72);
+                a6.sumRange(3, 7);
+                a6.sumRange(1, 8);
+                a6.update(5, 13);
+                a6.update(4, -67);
+            }
+        });
 
-    Add("310. Minimum Height Trees", [&]() {
-        auto check = [&](int n, const vector<vector<int>> &edges) {
-            vector<int> roots = findMinHeightTrees(n, edges);
-        };
-        check(4, {{1, 0}, {1, 2}, {1, 3}});
-    });
+    Add("310. Minimum Height Trees", [&]()
+        {
+            auto check = [&](int n, const vector<vector<int>> &edges)
+            {
+                vector<int> roots = findMinHeightTrees(n, edges);
+            };
+            check(4, {{1, 0}, {1, 2}, {1, 3}});
+        });
 
-    Add("315. Count of Smaller Numbers After Self", [&]() {
-        auto check = [&](const vector<int> &v) {
-            vector<int> c = countSmaller(v);
-            Logger() << "Input: " << v << endl;
-            Logger() << "Output: " << c << endl;
-        };
-        check({5, 2, 6, 1});
-    });
+    Add("315. Count of Smaller Numbers After Self", [&]()
+        {
+            auto check = [&](const vector<int> &v)
+            {
+                vector<int> c = countSmaller(v);
+                Logger() << "Input: " << v << endl;
+                Logger() << "Output: " << c << endl;
+            };
+            check({5, 2, 6, 1});
+        });
 
-    Add("321. Create Maximum Number", [&]() {
-        auto check = [&](const vector<int> &n1, const vector<int> &n2, int k) {
-            Logger() << "Input1: " << n1 << endl;
-            Logger() << "Input2: " << n2 << endl;
-            Logger() << "k = " << k << endl;
-            vector<int> m = maxNumber(n1, n2, k);
-            vector<int> m2 = maxNumber2(n1, n2, k);
-            Logger() << m << m2;
-            ASSERT1(Util::Compare(m, m2) == 0);
-        };
-        check({3, 4, 6, 5}, {9, 1, 2, 5, 8, 3}, 5);
-    });
+    Add("321. Create Maximum Number", [&]()
+        {
+            auto check = [&](const vector<int> &n1, const vector<int> &n2, int k)
+            {
+                Logger() << "Input1: " << n1 << endl;
+                Logger() << "Input2: " << n2 << endl;
+                Logger() << "k = " << k << endl;
+                vector<int> m = maxNumber(n1, n2, k);
+                vector<int> m2 = maxNumber2(n1, n2, k);
+                Logger() << m << m2;
+                ASSERT1(Util::Compare(m, m2) == 0);
+            };
+            check({3, 4, 6, 5}, {9, 1, 2, 5, 8, 3}, 5);
+        });
 
-    Add("322. Coin Change", [&]() {
-        auto check = [&](const vector<int> &coins, int amount) {
-            Logger() << "coins: " << coins;
-            Logger() << "amount: " << amount << endl;
-            int c = coinChange(coins, amount);
-            int c2 = coinChange2(coins, amount);
-            int c3 = coinChange3(coins, amount);
-            Logger() << "min count: " << c << ", " << c2 << ", " << c3 << endl;
-            ASSERT1(c == c2);
-            ASSERT1(c == c3);
-        };
-        check({336, 288, 378, 16, 319, 146}, 9212);
-    });
+    Add("322. Coin Change", [&]()
+        {
+            auto check = [&](const vector<int> &coins, int amount)
+            {
+                Logger() << "coins: " << coins;
+                Logger() << "amount: " << amount << endl;
+                int c = coinChange(coins, amount);
+                int c2 = coinChange2(coins, amount);
+                int c3 = coinChange3(coins, amount);
+                Logger() << "min count: " << c << ", " << c2 << ", " << c3 << endl;
+                ASSERT1(c == c2);
+                ASSERT1(c == c3);
+            };
+            check({336, 288, 378, 16, 319, 146}, 9212);
+        });
 
-    Add("330. Patching Array", [&]() {
-        auto check = [&](const vector<int> &nums, int n, int e) {
-            Logger() << nums;
-            Logger() << n;
-            int c = minPatches(nums, n);
-            Logger() << c << (c == e ? "==" : "!=") << e << endl;
-            ASSERT1(c == e);
-        };
-        check({1, 2, 31, 33}, 2147483647, 27);
-    });
+    Add("330. Patching Array", [&]()
+        {
+            auto check = [&](const vector<int> &nums, int n, int e)
+            {
+                Logger() << nums;
+                Logger() << n;
+                int c = minPatches(nums, n);
+                Logger() << c << (c == e ? "==" : "!=") << e << endl;
+                ASSERT1(c == e);
+            };
+            check({1, 2, 31, 33}, 2147483647, 27);
+        });
 
-    Add("331. Verify Preorder Serialization of a Binary Tree", [&]() {
-        auto check = [&](const string &p, bool e) {
-            bool r = isValidSerialization2(p);
-            ASSERT1(r == e);
-        };
-        check("9,3,4,#,#,1,#,#,2,#,6,#,#", true);
-    });
+    Add("331. Verify Preorder Serialization of a Binary Tree", [&]()
+        {
+            auto check = [&](const string &p, bool e)
+            {
+                bool r = isValidSerialization2(p);
+                ASSERT1(r == e);
+            };
+            check("9,3,4,#,#,1,#,#,2,#,6,#,#", true);
+        });
+
+    Add("341. Flatten Nested List Iterator", [&]()
+        {
+            auto check = [&](vector<NestedListIterator::NestedInteger> &list, const vector<int> &e)
+            {
+                NestedListIterator::NestedIterator it(list);
+                vector<int> r;
+                while (it.hasNext())
+                {
+                    r.push_back(it.next());
+                }
+                ASSERT1(0 == Util::Compare(r, e));
+            };
+            {
+                vector<NestedListIterator::NestedInteger> l;
+                vector<NestedListIterator::NestedInteger> l1;
+                l1.push_back(NestedListIterator::NestedInteger(1));
+                l1.push_back(NestedListIterator::NestedInteger(1));
+                l.push_back(NestedListIterator::NestedInteger(l1));
+                l.push_back(NestedListIterator::NestedInteger(2));
+                vector<NestedListIterator::NestedInteger> l2;
+                l2.push_back(NestedListIterator::NestedInteger(1));
+                l2.push_back(NestedListIterator::NestedInteger(1));
+                l.push_back(NestedListIterator::NestedInteger(l2));
+                check(l, {1, 1, 2, 1, 1});
+            }
+        });
 }
 
 #endif
