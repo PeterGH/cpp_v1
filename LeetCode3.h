a62421ba9354f62184c8c4c17b1d5e4bba2579dd
Peter <peteryzufl@gmail.com>
2021-11-07 10:48:37 -0800

    test

--- LeetCode3.h
+++ LeetCode3.h
@@ -5145,6 +5145,47 @@ namespace Test
             return s;
         }
 
+        // 365. Water and Jug Problem
+        // You are given two jugs with capacities jug1Capacity and jug2Capacity liters.
+        // There is an infinite amount of water supply available. Determine whether it
+        // is possible to measure exactly targetCapacity liters using these two jugs.
+        // If targetCapacity liters of water are measurable, you must have targetCapacity
+        // liters of water contained within one or both buckets by the end.
+        // Operations allowed:
+        // Fill any of the jugs with water.
+        // Empty any of the jugs.
+        // Pour water from one jug into another till the other jug is completely full,
+        // or the first jug itself is empty.
+        // Example 1:
+        // Input: jug1Capacity = 3, jug2Capacity = 5, targetCapacity = 4
+        // Output: true
+        // Explanation: The famous Die Hard example
+        // Example 2:
+        // Input: jug1Capacity = 2, jug2Capacity = 6, targetCapacity = 5
+        // Output: false
+        // Example 3:
+        // Input: jug1Capacity = 1, jug2Capacity = 2, targetCapacity = 3
+        // Output: true
+        // Constraints:
+        // 1 <= jug1Capacity, jug2Capacity, targetCapacity <= 10^6
+        bool canMeasureWater(int jug1Capacity, int jug2Capacity, int targetCapacity)
+        {
+            int x = jug1Capacity;
+            int y = jug2Capacity;
+            if (x < y)
+                swap(x, y);
+            // compute gcd
+            int z = x % y;
+            while (z != 0)
+            {
+                x = y;
+                y = z;
+                z = x % y;
+            }
+            // <= is required because in the end the water in two jugs must be the target
+            return targetCapacity <= (jug1Capacity + jug2Capacity) && (targetCapacity % y) == 0;
+        }
+
         // 367. Valid Perfect Square
         // Given a positive integer num, write a function which returns True if num is a
         // perfect square else False. Note: Do not use any built-in library function
