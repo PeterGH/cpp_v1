bbd4776266b25c28da7c5ac182562ed1876a6705
Peter <peteryzufl@gmail.com>
2021-11-16 20:22:17 -0800

    test

--- LeetCode3.h
+++ LeetCode3.h
@@ -5499,6 +5499,121 @@ namespace Test
             return n;
         }
 
+        // 373. Find K Pairs with Smallest Sums
+        // You are given two integer arrays nums1 and nums2 sorted in ascending
+        // order and an integer k. Define a pair (u, v) which consists of one element
+        // from the first array and one element from the second array.
+        // Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
+        // Example 1:
+        // Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+        // Output: [[1,2],[1,4],[1,6]]
+        // Explanation: The first 3 pairs are returned from the sequence:
+        // [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+        // Example 2:
+        // Input: nums1 = [1,1,2], nums2 = [1,2,3], k = 2
+        // Output: [[1,1],[1,1]]
+        // Explanation: The first 2 pairs are returned from the sequence:
+        // [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+        // Example 3:
+        // Input: nums1 = [1,2], nums2 = [3], k = 3
+        // Output: [[1,3],[2,3]]
+        // Explanation: All possible pairs are returned from the sequence: [1,3],[2,3]
+        // Constraints:
+        // 1 <= nums1.length, nums2.length <= 10^5
+        // -10^9 <= nums1[i], nums2[i] <= 10^9
+        // nums1 and nums2 both are sorted in ascending order.
+        // 1 <= k <= 1000
+        vector<vector<int>> kSmallestPairs(vector<int> &nums1, vector<int> &nums2, int k)
+        {
+            function<bool(const vector<int> &, const vector<int> &)>
+                comp = [&](const vector<int> &x, const vector<int> &y) -> bool
+            {
+                return x[0] + x[1] < y[0] + y[1];
+            };
+            // max heap
+            priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> q(comp);
+            int n1 = min(k, (int)nums1.size());
+            int n2 = min(k, (int)nums2.size());
+            for (int i = 0; i < n1; i++)
+            {
+                for (int j = 0; j < n2; j++)
+                {
+                    vector<int> p = {nums1[i], nums2[j]};
+                    if ((int)q.size() < k)
+                    {
+                        q.push(p);
+                    }
+                    else if (comp(p, q.top()))
+                    {
+                        q.pop();
+                        q.push(p);
+                    }
+                }
+            }
+            vector<vector<int>> output;
+            while (!q.empty())
+            {
+                output.insert(output.begin(), q.top());
+                q.pop();
+            }
+            return output;
+        }
+        vector<vector<int>> kSmallestPairs2(vector<int> &nums1, vector<int> &nums2, int k)
+        {
+            function<bool(const vector<int> &, const vector<int> &)>
+                comp = [&](const vector<int> &x, const vector<int> &y) -> bool
+            {
+                return nums1[x[0]] + nums2[x[1]] > nums1[y[0]] + nums2[y[1]];
+            };
+            // min heap
+            priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> q(comp);
+            q.push({0, 0});
+            vector<vector<int>> output;
+            while ((int)output.size() < k && !q.empty())
+            {
+                vector<int> t = q.top();
+                q.pop();
+                output.push_back({nums1[t[0]], nums2[t[1]]});
+                if (t[0] + 1 < (int)nums1.size())
+                {
+                    q.push({t[0] + 1, t[1]});
+                }
+                if (t[0] == 0 && t[1] + 1 < (int)nums2.size())
+                {
+                    q.push({t[0], t[1] + 1});
+                }
+            }
+            return output;
+        }
+        vector<vector<int>> kSmallestPairs3(vector<int> &nums1, vector<int> &nums2, int k)
+        {
+            struct comp
+            {
+                bool operator()(const vector<int> &x, const vector<int> &y) const
+                {
+                    return x[0] + x[1] < y[0] + y[1];
+                }
+            };
+            multiset<vector<int>, comp> s;
+            int n1 = min(k, (int)nums1.size());
+            int n2 = min(k, (int)nums2.size());
+            for (int i = 0; i < n1; i++)
+            {
+                for (int j = 0; j < n2; j++)
+                {
+                    s.insert(vector<int>{nums1[i], nums2[j]});
+                }
+            }
+            vector<vector<int>> output;
+            auto it = s.begin();
+            k = min(k, (int)s.size());
+            for (int i = 0; i < k; i++, ++it)
+            {
+                output.push_back(*it);
+            }
+            return output;
+        }
+
         // 374. Guess Number Higher or Lower
         // We are playing the Guess Game. The game is as follows:
         // I pick a number from 1 to n. You have to guess which number I picked.
