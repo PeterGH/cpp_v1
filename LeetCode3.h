4d50ab5f8bed517b9a8e3dfdd33e321d3c835b69
Peter <peteryzufl@gmail.com>
2021-11-17 21:30:33 -0800

    test

--- LeetCode3.h
+++ LeetCode3.h
@@ -5687,6 +5687,146 @@ namespace Test
             return guess(b, pick) == 0 ? b : e;
         }
 
+        // 375. Guess Number Higher or Lower II
+        // We are playing the Guessing Game. The game will work as follows:
+        // I pick a number between 1 and n.
+        // You guess a number.
+        // If you guess the right number, you win the game.
+        // If you guess the wrong number, then I will tell you whether the number
+        // I picked is higher or lower, and you will continue guessing.
+        // Every time you guess a wrong number x, you will pay x dollars.
+        // If you run out of money, you lose the game.
+        // Given a particular n, return the minimum amount of money you need to
+        // guarantee a win regardless of what number I pick.
+        // Example 1:
+        //        7
+        //      /   \
+        //     /     \
+        //    3       9
+        //  /   \    /  \
+        // 1     5  8   10
+        //  \   / \
+        //   2 4   6
+        // Input: n = 10
+        // Output: 16
+        // Explanation: The winning strategy is as follows:
+        // - The range is [1,10]. Guess 7.
+        //     - If this is my number, your total is $0. Otherwise, you pay $7.
+        //     - If my number is higher, the range is [8,10]. Guess 9.
+        //         - If this is my number, your total is $7. Otherwise, you pay $9.
+        //         - If my number is higher, it must be 10. Guess 10. Your total is $7 + $9 = $16.
+        //         - If my number is lower, it must be 8. Guess 8. Your total is $7 + $9 = $16.
+        //     - If my number is lower, the range is [1,6]. Guess 3.
+        //         - If this is my number, your total is $7. Otherwise, you pay $3.
+        //         - If my number is higher, the range is [4,6]. Guess 5.
+        //             - If this is my number, your total is $7 + $3 = $10. Otherwise, you pay $5.
+        //             - If my number is higher, it must be 6. Guess 6. Your total is $7 + $3 + $5 = $15.
+        //             - If my number is lower, it must be 4. Guess 4. Your total is $7 + $3 + $5 = $15.
+        //         - If my number is lower, the range is [1,2]. Guess 1.
+        //             - If this is my number, your total is $7 + $3 = $10. Otherwise, you pay $1.
+        //             - If my number is higher, it must be 2. Guess 2. Your total is $7 + $3 + $1 = $11.
+        // The worst case in all these scenarios is that you pay $16. Hence, you only need $16 to guarantee a win.
+        // Example 2:
+        // Input: n = 1
+        // Output: 0
+        // Explanation: There is only one possible number, so you can guess 1 and not have to pay anything.
+        // Example 3:
+        // Input: n = 2
+        // Output: 1
+        // Explanation: There are two possible numbers, 1 and 2.
+        // - Guess 1.
+        //     - If this is my number, your total is $0. Otherwise, you pay $1.
+        //     - If my number is higher, it must be 2. Guess 2. Your total is $1.
+        // The worst case is that you pay $1.
+        // Constraints:
+        // 1 <= n <= 200
+        //     ...
+        //       \
+        //       n-3
+        //      /    \
+        //     /      \
+        //   n-5      n-1
+        //   /  \    /   \
+        // n-6  n-4 n-2   n
+        int getMoneyAmount(int n)
+        {
+            map<int, map<int, int>> m;
+            function<int(int, int)> c = [&](int i, int j) -> int
+            {
+                if (i >= j)
+                    return 0;
+                // i
+                //  \
+                //  i+1
+                if (i + 1 == j)
+                    return i;
+                //  i+1
+                //  /  \
+                // i   i+2
+                if (i + 2 == j)
+                    return i + 1;
+                if (m.find(i) != m.end() && m[i].find(j) != m[i].end())
+                    return m[i][j];
+                int r = INT_MAX;
+                for (int k = i; k <= j; k++)
+                {
+                    // Removing this condition still work but more loop effort
+                    if (((n - k) & 1) == 0)
+                        continue;
+                    r = min(r, k + max(c(i, k - 1), c(k + 1, j)));
+                }
+                m[i][j] = r;
+                return r;
+            };
+            return c(1, n);
+        }
+        // i j   1      2      3      4    ......  n-1        n
+        // 1   c(1,1) c(1,2) c(1,3) c(1,4)       c(1,n-1)   c(1,n)
+        // 2          c(2,2) c(2,3) c(2,4)       c(2,n-1)   c(2,n)
+        // 3                 c(3,3) c(3,4)       c(3,n-1)   c(3,n)
+        // ......
+        // n-1                                   c(n-1,n-1) c(n-1,n)
+        // n                                                c(n,n)
+        // c(i,i) = 0
+        // c(i,i+1) = i
+        // c(i,i+2) = i+1
+        // c(i,j) = min({k + max(c(i,k-1), c(k+1,j))}) for k = i..j
+        int getMoneyAmount2(int n)
+        {
+            vector<vector<int>> c(n, vector<int>(n, 0));
+            // i and j are indices, so c[i][j] is the solution for range [i+1, j+1]
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = j; i >= 0; i--)
+                {
+                    if (i == j)
+                    {
+                        c[i][j] = 0; // [i+1, i+1]
+                    }
+                    else if (i + 1 == j)
+                    {
+                        c[i][j] = i + 1; // [i+1, i+2]
+                    }
+                    else if (i + 2 == j)
+                    {
+                        c[i][j] = i + 2; // [i+1, i+3]
+                    }
+                    else
+                    {
+                        // [i+1, j+1]
+                        int r = i + 1 + c[i + 1][j];
+                        for (int k = i + 1; k < j; k++)
+                        {
+                            r = min(r, k + 1 + max(c[i][k - 1], c[k + 1][j]));
+                        }
+                        r = min(r, j + 1 + c[i][j - 1]);
+                        c[i][j] = r;
+                    }
+                }
+            }
+            return c[0][n - 1];
+        }
+
     }
 }
 
