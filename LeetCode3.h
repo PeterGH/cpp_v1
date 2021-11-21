3dd5b2b164f3e4eb873e47709b51dfc5eb64f46e
Peter <peteryzufl@gmail.com>
2021-11-20 16:08:35 -0800

    test

--- LeetCode3.h
+++ LeetCode3.h
@@ -5880,11 +5880,59 @@ namespace Test
             wiggle(0, r);
             return m;
         }
+        int wiggleMaxLength2(vector<int> &nums)
+        {
+            if (nums.empty())
+                return 0;
+            vector<int> up(nums.size(), 1);
+            vector<int> down(nums.size(), 1);
+            for (size_t i = 1; i < nums.size(); i++)
+            {
+                for (size_t j = 0; j < i; j++)
+                {
+                    if (nums[j] < nums[i])
+                    {
+                        up[i] = max(up[i], down[j] + 1);
+                    }
+                    else if (nums[j] > nums[i])
+                    {
+                        down[i] = max(down[i], up[j] + 1);
+                    }
+                }
+            }
+            return max(up[nums.size() - 1], down[nums.size() - 1]);
+        }
+        int wiggleMaxLength3(vector<int> &nums)
+        {
+            if (nums.empty())
+                return 0;
+            vector<int> up(nums.size(), 1);
+            vector<int> down(nums.size(), 1);
+            for (size_t i = 1; i < nums.size(); i++)
+            {
+                if (nums[i - 1] < nums[i])
+                {
+                    up[i] = down[i - 1] + 1;
+                    down[i] = down[i - 1];
+                }
+                else if (nums[i - 1] > nums[i])
+                {
+                    down[i] = up[i - 1] + 1;
+                    up[i] = up[i - 1];
+                }
+                else
+                {
+                    up[i] = up[i - 1];
+                    down[i] = down[i - 1];
+                }
+            }
+            return max(up[nums.size() - 1], down[nums.size() - 1]);
+        }
         // wrong. e.g.
         // [33,53,12,64,50,41,45,21,97,35,47,92,39,0,93,55,40,46,69,42,6,95,51,68,72,9,32,84,34,64,6,2,26,98,3,43,30,60,3,68,82,9,97,19,27,98,99,4,30,96,37,9,78,43,64,4,65,30,84,90,87,64,18,50,60,1,40,32,48,50,76,100,57,29,63,53,46,57,93,98,42,80,82,9,41,55,69,84,82,79,30,79,18,97,67,23,52,38,74,15]
         // Output: 57
         // Expected: 67
-        int wiggleMaxLength2(vector<int> &nums)
+        int wiggleMaxLength4(vector<int> &nums)
         {
             vector<int> v;
             for (int n : nums)
