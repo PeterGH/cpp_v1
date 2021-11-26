8a4373852dac2f28a61a05e77951b7bf20d70a87
Peter <peteryzufl@gmail.com>
2021-11-26 11:39:49 -0800

    test

--- LeetCode3.h
+++ LeetCode3.h
@@ -6284,6 +6284,98 @@ namespace Test
             }
         };
 
+        // 381. Insert Delete GetRandom O(1) - Duplicates allowed
+        // RandomizedCollection is a data structure that contains a collection of numbers,
+        // possibly duplicates (i.e., a multiset). It should support inserting and removing
+        // specific elements and also removing a random element.
+        // Implement the RandomizedCollection class:
+        // RandomizedCollection() Initializes the empty RandomizedCollection object.
+        // bool insert(int val) Inserts an item val into the multiset, even if the item is
+        // already present. Returns true if the item is not present, false otherwise.
+        // bool remove(int val) Removes an item val from the multiset if present. Returns
+        // true if the item is present, false otherwise. Note that if val has multiple
+        // occurrences in the multiset, we only remove one of them.
+        // int getRandom() Returns a random element from the current multiset of elements.
+        // The probability of each element being returned is linearly related to the number
+        // of same values the multiset contains.
+        // You must implement the functions of the class such that each function works on
+        // average O(1) time complexity.
+        // Note: The test cases are generated such that getRandom will only be called if
+        // there is at least one item in the RandomizedCollection.
+        // Example 1:
+        // Input
+        // ["RandomizedCollection", "insert", "insert", "insert", "getRandom", "remove", "getRandom"]
+        // [[], [1], [1], [2], [], [1], []]
+        // Output
+        // [null, true, false, true, 2, true, 1]
+        // Explanation
+        // RandomizedCollection randomizedCollection = new RandomizedCollection();
+        // randomizedCollection.insert(1);   // return true since the collection does not contain 1.
+        //                                   // Inserts 1 into the collection.
+        // randomizedCollection.insert(1);   // return false since the collection contains 1.
+        //                                   // Inserts another 1 into the collection. Collection now contains [1,1].
+        // randomizedCollection.insert(2);   // return true since the collection does not contain 2.
+        //                                   // Inserts 2 into the collection. Collection now contains [1,1,2].
+        // randomizedCollection.getRandom(); // getRandom should:
+        //                                   // - return 1 with probability 2/3, or
+        //                                   // - return 2 with probability 1/3.
+        // randomizedCollection.remove(1);   // return true since the collection contains 1.
+        //                                   // Removes 1 from the collection. Collection now contains [1,2].
+        // randomizedCollection.getRandom(); // getRandom should return 1 or 2, both equally likely.
+        // Constraints:
+        // -2^31 <= val <= 2^31 - 1
+        // At most 2 * 10^5 calls in total will be made to insert, remove, and getRandom.
+        // There will be at least one element in the data structure when getRandom is called.
+        class RandomizedCollection
+        {
+        private:
+            vector<int> value;
+            unordered_map<int, unordered_set<size_t>> index;
+
+        public:
+            RandomizedCollection()
+            {
+            }
+            bool insert(int val)
+            {
+                value.push_back(val);
+                size_t i = value.size() - 1;
+                if (index.find(val) == index.end())
+                {
+                    index[val] = {i};
+                    return true;
+                }
+                index[val].insert(i);
+                return false;
+            }
+            bool remove(int val)
+            {
+                if (index.find(val) == index.end())
+                    return false;
+                size_t i = *index[val].begin();
+                size_t n = value.size();
+                int e = value[n - 1];
+                if (i + 1 < n && val != e)
+                {
+                    value[i] = e;
+                    index[e].erase(n - 1);
+                    index[e].insert(i);
+                }
+                if (val != e || i + 1 == n)
+                    index[val].erase(i);
+                else
+                    index[val].erase(n - 1);
+                if (index[val].empty())
+                    index.erase(val);
+                value.pop_back();
+                return true;
+            }
+            int getRandom()
+            {
+                return value[rand() % value.size()];
+            }
+        };
+
     }
 }
 
