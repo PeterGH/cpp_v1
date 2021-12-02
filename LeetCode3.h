a5b58852952e5497f155a2736ef00fff6f758bb4
Peter <peteryzufl@gmail.com>
2021-12-01 20:09:36 -0800

    test

--- LeetCode3.h
+++ LeetCode3.h
@@ -6521,6 +6521,59 @@ namespace Test
             return true;
         }
 
+        // 384. Shuffle an Array
+        // Given an integer array nums, design an algorithm to randomly shuffle the array.
+        // All permutations of the array should be equally likely as a result of the shuffling.
+        // Implement the Solution class:
+        // Solution(int[] nums) Initializes the object with the integer array nums.
+        // int[] reset() Resets the array to its original configuration and returns it.
+        // int[] shuffle() Returns a random shuffling of the array.
+        // Example 1:
+        // Input
+        // ["Solution", "shuffle", "reset", "shuffle"]
+        // [[[1, 2, 3]], [], [], []]
+        // Output
+        // [null, [3, 1, 2], [1, 2, 3], [1, 3, 2]]
+        // Explanation
+        // Solution solution = new Solution([1, 2, 3]);
+        // solution.shuffle();    // Shuffle the array [1,2,3] and return its result.
+        //                        // Any permutation of [1,2,3] must be equally likely to be returned.
+        //                        // Example: return [3, 1, 2]
+        // solution.reset();      // Resets the array back to its original configuration [1,2,3]. Return [1, 2, 3]
+        // solution.shuffle();    // Returns the random shuffling of array [1,2,3]. Example: return [1, 3, 2]
+        // Constraints:
+        // 1 <= nums.length <= 200
+        // -10^6 <= nums[i] <= 10^6
+        // All the elements of nums are unique.
+        // At most 5 * 10^4 calls in total will be made to reset and shuffle.
+        class ShuffleAnArray
+        {
+        private:
+            vector<int> v;
+
+        public:
+            ShuffleAnArray(vector<int> &nums)
+            {
+                v.insert(v.begin(), nums.begin(), nums.end());
+            }
+            vector<int> reset()
+            {
+                return v;
+            }
+            // Fisher-Yates algorithm
+            vector<int> shuffle()
+            {
+                vector<int> w(v);
+                int n = w.size();
+                for (int i = 0; i < n; i++)
+                {
+                    int j = i + (rand() % (n - i));
+                    swap(w[i], w[j]);
+                }
+                return w;
+            }
+        };
+
     }
 }
 
