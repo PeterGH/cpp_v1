9eec8007312a49df8abafefa6c2ae4c6f253d1cb
Peter <peteryzufl@gmail.com>
2021-12-03 19:50:12 -0800

    test

--- LeetCode3.h
+++ LeetCode3.h
@@ -6574,6 +6574,99 @@ namespace Test
             }
         };
 
+        // 385. Mini Parser
+        // Given a string s represents the serialization of a nested list, implement a
+        // parser to deserialize it and return the deserialized NestedInteger.
+        // Each element is either an integer or a list whose elements may also be integers or other lists.
+        // Example 1:
+        // Input: s = "324"
+        // Output: 324
+        // Explanation: You should return a NestedInteger object which contains a single integer 324.
+        // Example 2:
+        // Input: s = "[123,[456,[789]]]"
+        // Output: [123,[456,[789]]]
+        // Explanation: Return a NestedInteger object containing a nested list with 2 elements:
+        // 1. An integer containing value 123.
+        // 2. A nested list containing two elements:
+        //     i.  An integer containing value 456.
+        //     ii. A nested list with one element:
+        //          a. An integer containing value 789
+        // Constraints:
+        // 1 <= s.length <= 5 * 10^4
+        // s consists of digits, square brackets "[]", negative sign '-', and commas ','.
+        // s is the serialization of valid NestedInteger.
+        // All the values in the input are in the range [-10^6, 10^6].
+        class MiniParser
+        {
+        public:
+            class NestedInteger
+            {
+            private:
+                int v;
+                vector<NestedInteger> list;
+
+            public:
+                // Constructor initializes an empty nested list.
+                NestedInteger();
+                // Constructor initializes a single integer.
+                NestedInteger(int value) { v = value; }
+                // Return true if this NestedInteger holds a single integer, rather than a nested list.
+                bool isInteger() const { return list.empty(); }
+                // Return the single integer that this NestedInteger holds, if it holds a single integer
+                // The result is undefined if this NestedInteger holds a nested list
+                int getInteger() const { return v; }
+                // Set this NestedInteger to hold a single integer.
+                void setInteger(int value) { v = value; }
+                // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+                void add(const NestedInteger &ni) { list.push_back(ni); }
+                // Return the nested list that this NestedInteger holds, if it holds a nested list
+                // The result is undefined if this NestedInteger holds a single integer
+                const vector<NestedInteger> &getList() const { return list; }
+            };
+
+            NestedInteger deserialize(string s)
+            {
+                function<NestedInteger(size_t &)> parse = [&](size_t &i) -> NestedInteger
+                {
+                    NestedInteger n;
+                    if (s[i] == '-' || ('0' <= s[i] && s[i] <= '9'))
+                    {
+                        int v = 0;
+                        bool negative = false;
+                        if (s[i] == '-')
+                        {
+                            negative = true;
+                            i++;
+                        }
+                        while (i < s.size() && '0' <= s[i] && s[i] <= '9')
+                        {
+                            v = 10 * v + s[i] - '0';
+                            i++;
+                        }
+                        if (negative)
+                            v = -v;
+                        n.setInteger(v);
+                    }
+                    else // '['
+                    {
+                        i++;
+                        while (i < s.size() && s[i] != ']')
+                        {
+                            NestedInteger e = parse(i);
+                            n.add(e);
+                            if (i < s.size() && s[i] == ',')
+                                i++;
+                        }
+                        if (i < s.size())
+                            i++;
+                    }
+                    return n;
+                };
+                size_t b = 0;
+                return parse(b);
+            }
+        };
+
     }
 }
 
