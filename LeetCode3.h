a0fca92eaab770305fc807a168a800dfb022e39a
Peter <peteryzufl@gmail.com>
2021-12-27 14:54:50 -0800

    test

--- LeetCode3.h
+++ LeetCode3.h
@@ -8066,6 +8066,58 @@ namespace Test
             return o;
         }
 
+        // 396. Rotate Function
+        // You are given an integer array nums of length n.
+        // Assume arrk to be an array obtained by rotating nums by k positions clock-wise.
+        // We define the rotation function F on nums as follow:
+        // F(k) = 0 * arrk[0] + 1 * arrk[1] + ... + (n - 1) * arrk[n - 1].
+        // Return the maximum value of F(0), F(1), ..., F(n-1).
+        // The test cases are generated so that the answer fits in a 32-bit integer.
+        // Example 1:
+        // Input: nums = [4,3,2,6]
+        // Output: 26
+        // Explanation:
+        // F(0) = (0 * 4) + (1 * 3) + (2 * 2) + (3 * 6) = 0 + 3 + 4 + 18 = 25
+        // F(1) = (0 * 6) + (1 * 4) + (2 * 3) + (3 * 2) = 0 + 4 + 6 + 6 = 16
+        // F(2) = (0 * 2) + (1 * 6) + (2 * 4) + (3 * 3) = 0 + 6 + 8 + 9 = 23
+        // F(3) = (0 * 3) + (1 * 2) + (2 * 6) + (3 * 4) = 0 + 2 + 12 + 12 = 26
+        // So the maximum value of F(0), F(1), F(2), F(3) is F(3) = 26.
+        // Example 2:
+        // Input: nums = [100]
+        // Output: 0
+        // Constraints:
+        // n == nums.length
+        // 1 <= n <= 10^5
+        // -100 <= nums[i] <= 100
+        // 0    a[0]   a[1]     a[2]     ... a[k-2] a[k-1] a[k]   a[k+1] ... a[n-k-1] a[n-k]   a[n-k+1]  ... a[n-3]   a[n-2]   a[n-1]
+        // 1    a[n-1] a[0]     a[1]     ... a[k-3] a[k-2] a[k-1] a[k]   ... a[n-k-2] a[n-k-1] a[n-k]    ... a[n-4]   a[n-3]   a[n-2]
+        // ...
+        // k    a[n-k] a[n-k+1] a[n-k+2] ... a[n-2] a[n-1] a[0]   a[1]   ... a[n-2k-1] a[n-2k] a[n-2k+1] ... a[n-k-3] a[n-k-2] a[n-k-1]
+        // k+1  a[n-k-1] a[n-k] a[n-k+1] ... a[n-3] a[n-2] a[n-1] a[0]   ... a[n-2k-2] a[n-2k-1] a[n-2k] ... a[n-k-4] a[n-k-3] a[n-k-2]
+        // F(k) =   0*a[n-k]   + 1*a[n-k+1] + 2*a[n-k+2] + ... + (k-1)*a[n-1] + k*a[0]     + ... + (n-2)*a[n-k-2] + (n-1)*a[n-k-1]
+        // F(k+1) = 0*a[n-k-1] + 1*a[n-k]   + 2*a[n-k+1] + ... + k*a[n-1]     + (k+1)*a[0] + ... + (n-2)*a[n-k-3] + (n-1)*a[n-k-2]
+        //        = 0*a[n-k-1] + a[n-k] + a[n-k+1] + ... + a[n-1] + a[0] + ... + a[n-k-3] + a[n-k-2] + F(k) - (n-1)*a[n-k-1]
+        //        = a[n-k-1]   + a[n-k] + a[n-k+1] + ... + a[n-1] + a[0] + ... + a[n-k-3] + a[n-k-2] + F(k) - n*a[n-k-1]
+        //        = Sum(a[0..(n-1)]) + F(k) + n * a[n-(k+1)]
+        int maxRotateFunction(const vector<int> &nums)
+        {
+            long long s = 0;
+            long long f = 0;
+            int n = nums.size();
+            for (int i = 0; i < n; i++)
+            {
+                s += nums[i];
+                f += i * nums[i]; // F(0)
+            }
+            long long m = f;
+            for (int k = 1; k < n; k++)
+            {
+                f = s + f - n * nums[n - k];
+                m = max(m, f);
+            }
+            return m;
+        }
+
     }
 }
 
