3d98a154447b774b7cb418fd5637ece20981e76d
Peter <peteryzufl@gmail.com>
2022-01-15 13:26:48 -0800

    test

--- LeetCode4.h
+++ LeetCode4.h
@@ -494,6 +494,104 @@ namespace Test
             return o;
         }
 
+        // 406. Queue Reconstruction by Height
+        // You are given an array of people, people, which are the attributes of some
+        // people in a queue (not necessarily in order). Each people[i] = [hi, ki] represents
+        // the ith person of height hi with exactly ki other people in front who have a
+        // height greater than or equal to hi.
+        // Reconstruct and return the queue that is represented by the input array people.
+        // The returned queue should be formatted as an array queue, where queue[j] = [hj, kj]
+        // is the attributes of the jth person in the queue (queue[0] is the person at the front of the queue).
+        // Example 1:
+        // Input: people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
+        // Output: [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
+        // Explanation:
+        // Person 0 has height 5 with no other people taller or the same height in front.
+        // Person 1 has height 7 with no other people taller or the same height in front.
+        // Person 2 has height 5 with two persons taller or the same height in front, which is person 0 and 1.
+        // Person 3 has height 6 with one person taller or the same height in front, which is person 1.
+        // Person 4 has height 4 with four people taller or the same height in front, which are people 0, 1, 2, and 3.
+        // Person 5 has height 7 with one person taller or the same height in front, which is person 1.
+        // Hence [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]] is the reconstructed queue.
+        // Example 2:
+        // Input: people = [[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]]
+        // Output: [[4,0],[5,0],[2,2],[3,2],[1,4],[6,0]]
+        // Constraints:
+        // 1 <= people.length <= 2000
+        // 0 <= hi <= 10^6
+        // 0 <= ki < people.length
+        // It is guaranteed that the queue can be reconstructed.
+        // Output people who have no others in front, i.e., ki = 0.
+        // For the rest of perople, decrease their counters by the count of output people who
+        // are taller than or same height as them. Any people in rest with counter = 0 means
+        // they need to be moved to the output queue.
+        vector<vector<int>> reconstructQueue(const vector<vector<int>> &people)
+        {
+            vector<vector<int>> output;
+            vector<vector<int>> input;
+            for (const auto &p : people)
+            {
+                if (p[1] == 0)
+                    output.push_back(p);
+                else
+                    input.push_back({p[0], p[1], p[1]});
+            }
+            sort(output.begin(), output.end());
+            auto l = output.begin();
+            vector<vector<int>> a;
+            while (!input.empty() && l != output.end())
+            {
+                auto p = input.begin();
+                while (p != input.end())
+                {
+                    if ((*l)[0] >= (*p)[0])
+                        (*p)[2]--;
+                    if ((*p)[2] == 0)
+                    {
+                        a.push_back({(*p)[0], (*p)[1]});
+                        p = input.erase(p);
+                    }
+                    else
+                    {
+                        p++;
+                    }
+                }
+                if (a.empty())
+                {
+                    l++;
+                }
+                else
+                {
+                    sort(a.begin(), a.end());
+                    // instead of l++, use iterator returned by insert.
+                    // insert updates the vector and thus invalidates the original l.
+                    l = output.insert(l + 1, a.begin(), a.end());
+                    a.clear();
+                }
+            }
+            return output;
+        }
+        // wrong
+        vector<vector<int>> reconstructQueue2(vector<vector<int>> &people)
+        {
+            vector<size_t> index(people.size(), 0);
+            for (size_t i = 0; i < people.size(); i++)
+                index[i] = i;
+            sort(people.begin(), people.end(), [&](const vector<int> &x, const vector<int> &y) -> bool
+                 {
+                     if (x[0] == y[0])
+                         return x[1] < y[1];
+                     return x[0] < y[0];
+                 });
+            vector<vector<int>> output(people.size());
+            for (const auto &p : people)
+            {
+                output[index[p[1]]] = p;
+                index.erase(index.begin() + p[1]);
+            }
+            return output;
+        }
+
         // 410. Split Array Largest Sum
         // Given an array which consists of non-negative integers and an integer m, you
         // can split the array into m non-empty continuous subarrays. Write an algorithm
