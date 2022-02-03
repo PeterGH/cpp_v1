ece92c0e4e09a99b3903e177f0ad057f392eebbe
Peter <peteryzufl@gmail.com>
2022-02-02 21:59:31 -0800

    test

--- LeetCode3.h
+++ LeetCode3.h
@@ -1859,8 +1859,8 @@ namespace Test
                     }
                     else
                     {
-                        int c = count(i + 1, a);
-                        if (a >= coins[i])
+                        int c = count(i + 1, a); // coins[i] excluded
+                        if (a >= coins[i])       // coins[i] included
                         {
                             int d = coins[i];
                             int c1 = 1;
@@ -1893,7 +1893,7 @@ namespace Test
                 if (a == 0)
                     return 0;
                 if (change[a - 1] != 0)
-                    return change[a - 1];
+                    return change[a - 1]; // amount a is at index a - 1
                 int r = -1;
                 for (int c : coins)
                 {
@@ -1920,6 +1920,176 @@ namespace Test
             }
             return count[amount] > amount ? -1 : count[amount];
         }
+        namespace CoinChange
+        {
+            // Given an amount n and a set of coin denominations:
+            // d_0, d_1, d_2, ......, d_(k-1)
+            // The minimum count of coins to make up n:
+            // N(n) = 1 + min{N(n - d_0), N(n - d_1), ..., N(n - d_(k-1))}
+            static void ComputeMinCounts(unsigned int amount,
+                                         vector<unsigned int> &denominations,
+                                         vector<pair<unsigned int, unsigned int>> &counts)
+            {
+                // Sort coins increasingly
+                sort(denominations.begin(), denominations.end());
+                // counts[i] tracks the min count to make up amount i, and
+                // the last change coin picked.
+                counts.push_back(make_pair(0, 0));
+                counts.push_back(make_pair(1, 1));
+                for (unsigned int i = 2; i <= amount; i++)
+                {
+                    unsigned int minCount = amount;
+                    unsigned int change;
+                    unsigned int j = 0;
+                    while (j < denominations.size() && denominations[j] <= i)
+                    {
+                        if (counts[i - denominations[j]].first < minCount)
+                        {
+                            minCount = counts[i - denominations[j]].first;
+                            change = denominations[j];
+                        }
+                        j++;
+                    }
+                    counts.push_back(make_pair(1 + minCount, change));
+                }
+            }
+            static void ComputeSolution(unsigned int amount,
+                                        vector<unsigned int> &denominations,
+                                        map<unsigned int, unsigned int> &changes)
+            {
+                vector<pair<unsigned int, unsigned int>> counts;
+                ComputeMinCounts(amount, denominations, counts);
+                int i = amount;
+                while (i > 0)
+                {
+                    unsigned int c = counts[i].second;
+                    if (changes.find(c) == changes.end())
+                    {
+                        changes[c] = 1;
+                    }
+                    else
+                    {
+                        changes[c]++;
+                    }
+                    i -= (int)c;
+                }
+            }
+            static unsigned int GreedySolution(unsigned int amount,
+                                               vector<unsigned int> &denominations,
+                                               map<unsigned int, unsigned int> &changes)
+            {
+                function<bool(unsigned int, unsigned int)> greater =
+                    [&](unsigned int first, unsigned int second) -> bool
+                {
+                    return first > second;
+                };
+                // Sort in decreasing order
+                sort(denominations.begin(), denominations.end(), greater);
+                for_each(denominations.begin(), denominations.end(), [&](unsigned int d)
+                         {
+                             if (amount == 0)
+                                 return;
+                             if (amount >= d)
+                             {
+                                 unsigned int count = amount / d;
+                                 changes[d] = count;
+                                 amount = amount % d;
+                             }
+                         });
+
+                // Return what ever cannot be changed, e.g. the minimum denomination is
+                // not 1.
+                return amount;
+            }
+            // http://leetcode.com/2010/09/print-all-combinations-of-number-as-sum.html
+            // Compute all sets of changes sum up to a given ammount
+            static void
+            ComputeAllSolutions(unsigned int amount, vector<unsigned int> &denominations,
+                                vector<map<unsigned int, unsigned int>> &changes)
+            {
+                // [TODO] no need to sort if use minIndex instead of minD in compute()
+                sort(denominations.begin(), denominations.end());
+                function<void(int, unsigned int, map<unsigned int, unsigned int> &)>
+                    compute = [&](int sum, unsigned int minD,
+                                  map<unsigned int, unsigned int> &s)
+                {
+                    for_each(denominations.begin(), denominations.end(),
+                             [&](unsigned int d)
+                             {
+                                 if (d >= minD && sum >= (int)d)
+                                 {
+                                     // Since denominations is already sorted, we can
+                                     // use minD as a gate to avoid to recompute
+                                     // solutions with smaller denoms.
+                                     map<unsigned int, unsigned int> copy(s);
+                                     if (copy.find(d) == copy.end())
+                                     {
+                                         copy[d] = 1;
+                                     }
+                                     else
+                                     {
+                                         copy[d]++;
+                                     }
+                                     if (sum == (int)d)
+                                     {
+                                         changes.push_back(copy);
+                                     }
+                                     else
+                                     {
+                                         compute(sum - d, d, copy);
+                                     }
+                                 }
+                             });
+                };
+                map<unsigned int, unsigned int> s;
+                compute(amount, denominations.front(), s);
+            }
+            // http://leetcode.com/2010/09/print-all-combinations-of-number-as-sum.html
+            // Compute all sub sets of changes sum up to a given ammount.
+            // Duplication of a change is disallowed.
+            static void
+            ComputeSubSetSolutions(unsigned int amount, vector<unsigned int> &denominations,
+                                   vector<map<unsigned int, unsigned int>> &changes)
+            {
+                // [TODO] no need to sort if use minIndex instead of minD in compute()
+                sort(denominations.begin(), denominations.end());
+                function<void(int, unsigned int, map<unsigned int, unsigned int> &)>
+                    compute = [&](int sum, unsigned int minD,
+                                  map<unsigned int, unsigned int> &s)
+                {
+                    for_each(denominations.begin(), denominations.end(),
+                             [&](unsigned int d)
+                             {
+                                 if (d >= minD && sum >= (int)d)
+                                 {
+                                     // Since denominations is already sorted, we can
+                                     // use minD as a gate to avoid to recompute
+                                     // solutions with smaller denoms.
+                                     map<unsigned int, unsigned int> copy(s);
+                                     if (copy.find(d) == copy.end())
+                                     {
+                                         copy[d] = 1;
+                                     }
+                                     else
+                                     {
+                                         copy[d]++;
+                                     }
+                                     if (sum == (int)d)
+                                     {
+                                         changes.push_back(copy);
+                                     }
+                                     else
+                                     {
+                                         compute(sum - d, d + 1, copy);
+                                     }
+                                 }
+                             });
+                };
+
+                map<unsigned int, unsigned int> s;
+                compute(amount, denominations.front(), s);
+            }
+        } // namespace CoinChange
 
         // 324. Wiggle Sort II
         // Given an integer array nums, reorder it such that nums[0] < nums[1] > nums[2] < nums[3] ...
