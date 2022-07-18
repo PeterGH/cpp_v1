2926beb4ee25296632f2955c419e9da54971e67f
Peter <peteryzufl@gmail.com>
2022-07-17 19:20:12 -0700

    test

--- Algorithm.h
+++ Algorithm.h
@@ -318,6 +318,192 @@ namespace Test
         return index;
     }
 
+    // Find the median of a sorted array.
+    // If the array length is odd, then the median is unique.
+    // If the array length is even, then return the lower median.
+    // Lower median or single median is (L - 1) / 2
+    // Higher median or single median is L / 2
+    template <class T>
+    static T FindMedian(const T *input, int length)
+    {
+        if (input == nullptr)
+            throw invalid_argument("input is a nullptr");
+        if (length <= 0)
+            throw invalid_argument(String::Format("length %d is invalid", length));
+
+        // If l = 2m
+        // +----------------+  +------------------+
+        // 0               m-1 m                 2m-1
+        // The median index is m-1 = (l - 1) / 2, rounding to the lower boundary
+        //
+        // If l = 2m-1
+        // +----------------+   +   +------------------+
+        // 0               m-2 m-1  m                 2m-2
+        // The median index is m-1 = (l - 1) / 2
+
+        int medianIndex = (length - 1) >> 1;
+        return input[medianIndex];
+    }
+
+    // Find the median from two sorted arrays.
+    // If the two arrays contain odd number of elements, then the median is unique.
+    // If the two arrays contain even number of elements, then return the lower
+    // median.
+    template <class T>
+    static T FindMedian(const T *input1, int length1, const T *input2,
+                        int length2)
+    {
+        if (input1 == nullptr)
+            throw invalid_argument("input1 is a nullptr");
+        if (length1 <= 0)
+            throw invalid_argument(
+                String::Format("length1 %d is invalid", length1));
+        if (input2 == nullptr)
+            throw invalid_argument("input2 is a nullptr");
+        if (length2 <= 0)
+            throw invalid_argument(
+                String::Format("length2 %d is invalid", length2));
+
+        const T *shortArray;
+        int shortLength;
+        const T *longArray;
+        int longLength;
+
+        if (length1 <= length2)
+        {
+            shortLength = length1;
+            shortArray = input1;
+            longLength = length2;
+            longArray = input2;
+        }
+        else
+        {
+            shortLength = length2;
+            shortArray = input2;
+            longLength = length1;
+            longArray = input1;
+        }
+
+        int medianIndex = (shortLength + longLength - 1) / 2;
+        if (shortArray[shortLength - 1] <= longArray[0])
+        {
+            if (medianIndex == shortLength - 1)
+            {
+                // +---------------------------------------+
+                // +---------------------------------------+
+                // 0 s-1 0 l-1
+                // s = l
+                // m = s - 1 = l - 1
+                return shortArray[medianIndex];
+            }
+            else
+            {
+                // +---------------------------------------+
+                // +-----+------------------------------------------+
+                // 0 s-1 0 m-s l-1
+                // s < l
+                // m = (s + l - 1) / 2
+                //   = k - 1    if (s + l) = 2k
+                //     k - 1    if (s + l) = 2k - 1
+                // s - 1 < m < l - 1
+                return longArray[medianIndex - shortLength];
+            }
+        }
+        else if (longArray[longLength - 1] <= shortArray[0])
+        {
+            // +---------------------------------------+---------+
+            // +---------------------------------------+
+            // 0 m l-1 0 s-1
+            // s <= l
+            // m = (s + l - 1) / 2
+            //   = k - 1    if (s + l) = 2k
+            //     k - 1    if (s + l) = 2k - 1
+            // s - 1 <= m <= l - 1
+            return longArray[medianIndex];
+        }
+
+        // +---------------------------------------+
+        // 0             m-1 m m+1                s-1
+        // +-----------------------------------------------+
+        // 0             n-1 n n+1                        l-1
+        //
+        // m + n = medianIndex - 1
+        // Median =
+        // S[m] if L[n] < S[m] <= L[n+1], i.e. S[m] is the lower median and
+        //      L[n+1] is the higher median
+        // L[n] if S[m] < L[n] <= S[m+1], i.e. L[m] is the lower median and
+        //      S[n+1] is the higher median
+        // S[m] if S[m] == L[n], i.e. S[m] or L[n] is the lower median and
+        //      min(S[m+1], L[n+1]) is the higher median
+        // max(S[m-1], L[n+1]) if S[m] > L[n+1]
+        // max(S[m+1], L[n-1]) if L[n] > S[m+1]
+
+        int low = 0;
+        int high = shortLength - 1;
+        while (true)
+        {
+            // (1) If low < high - 1, then low < m < high
+            // (2) If low = high - 1, then low = m < high
+            // (3) If low = high, then low = m = high
+            int m = (low + high) >> 1;
+            int n = medianIndex - m - 1;
+            if (shortArray[m] > longArray[n])
+            {
+                if (shortArray[m] <= longArray[n + 1])
+                {
+                    return shortArray[m];
+                }
+                else
+                {
+                    if (m < high)
+                    {
+                        // Case (1) or (2)
+                        high = m;
+                    }
+                    else
+                    {
+                        // Case (3)
+                        // s[m] > l[n] and s[m] > l[n+1]
+                        // Compare s[m-1] and l[n+1] as one of them is the median
+                        if (m - 1 >= 0 && shortArray[m - 1] > longArray[n + 1])
+                            return shortArray[m - 1];
+                        else
+                            return longArray[n + 1];
+                    }
+                }
+            }
+            else if (longArray[n] > shortArray[m])
+            {
+                if (longArray[n] <= shortArray[m + 1])
+                {
+                    return longArray[n];
+                }
+                else
+                {
+                    if (low < m)
+                    {
+                        // Case (1)
+                        low = m;
+                    }
+                    else
+                    {
+                        // Case (2) or (3)
+                        // l[n] > s[m] and l[n] > s[m+1]
+                        // Compare l[n-1] and s[m+1]
+                        if (n - 1 >= 0 && longArray[n - 1] > shortArray[m + 1])
+                            return longArray[n - 1];
+                        else
+                            return shortArray[m + 1];
+                    }
+                }
+            }
+            else
+            {
+                return shortArray[m];
+            }
+        }
+    }
+
     class KMP
     {
     private:
